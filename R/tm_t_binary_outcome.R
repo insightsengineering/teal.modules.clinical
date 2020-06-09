63c46ff37e862b9a3880060fa168c1748995fe98
Engel-Wolf, Sebastian {MDBB~Basel} <sebastian.engel-wolf@roche.com>
2020-06-09 14:04:25 +0200

    remobe import (#360)

--- R/tm_t_binary_outcome.R
+++ R/tm_t_binary_outcome.R
@@ -329,7 +329,7 @@ srv_t_binary_outcome <- function(input,
       arm_var = arm_var
     )
 
-    if (uniqueN(adsl_filtered[[arm_var]]) == 1) {
+    if (length(unique(adsl_filtered[[arm_var]])) == 1) {
       validate_args <- append(validate_args, list(min_n_levels_armvar = NULL))
       if (compare_arms) {
         validate_args <- append(validate_args, list(ref_arm = ref_arm))
