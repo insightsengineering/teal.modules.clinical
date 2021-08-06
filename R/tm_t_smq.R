5534ebcd6d2cd39ceb34769809b19d4f513d3f31
imazubi <imanol.zubizarreta@businesspartner.roche.com>
2021-08-06 16:48:41 +0200

    first version of module, not working yet

--- R/tm_t_smq.R
+++ R/tm_t_smq.R
@@ -11,7 +11,6 @@
 #' @return
 #' @export
 #'
-#' @examples
 template_smq <- function(
   parentname,
   dataname,
@@ -261,7 +260,88 @@ layout_list <- add_expr(
 
 }
 
-
+#' Title
+#'
+#' @param label
+#' @param dataname
+#' @param parentname
+#' @param arm_var
+#' @param id_var
+#' @param col_by_var
+#' @param llt
+#' @param add_total
+#' @param drop_arm_levels
+#' @param na_level
+#' @param smq_varlabel
+#' @param baskets
+#' @param keys
+#' @param sort_by_descending
+#' @param pre_output
+#' @param post_output
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' library(scda)
+#'
+#'
+#' adsl <- synthetic_cdisc_data("latest")$adsl
+#' adae <- synthetic_cdisc_data("latest")$adae
+#'
+#' names_baskets <- grep("^(SMQ|CQ).*NAM$", names(adae), value = TRUE)
+#' names_scopes <- grep("^SMQ.*SC$", names(adae), value = TRUE)
+#'
+#' cs_baskets <- choices_selected(
+#' choices = variable_choices(adae, subset = names_baskets),
+#' selected = names_baskets
+#' )
+#'
+#' cs_scopes <- choices_selected(
+#' choices = variable_choices(adae, subset = names_scopes),
+#' selected = names_scopes
+#' )
+#'
+#'
+#'
+#' app <- init(
+#'   data = cdisc_data(
+#'     cdisc_dataset("ADSL", adsl,
+#'     code = 'ADSL <- radsl(cached = TRUE)'),
+#'     cdisc_dataset("ADLB", adae,
+#'       code = 'ADAE <- synthetic_cdisc_data("latest")$adae'
+#'       )
+#'     ),
+#'     check = TRUE
+#'   ),
+#'   modules = root_modules(
+#'     tm_t_abnormality(
+#'       label = "Adverse events by SMQ Table",
+#'       dataname = "ADAE",
+#'       arm_var = choices_selected(
+#'         choices = variable_choices(adsl, subset = c("ARM", "ARMCD")),
+#'         selected = "ARM"
+#'       ),
+#'       add_total = FALSE,
+#'       col_by_var = choices_selected(
+#'         choices = variable_choices(adae, subset = c("SEX", "STRATA1")),
+#'         selected = NULL
+#'       ),
+#'       baskets = cs_baskets,
+#'       scopes = cs_scopes,
+#'       llt = choices_selected(
+#'         choices = variable_choices(adae, subset = c("AEDECOD")),
+#'         selected = "AEDECOD"
+#'       )
+#'     )
+#'   )
+#' )
+#'
+#' \dontrun{
+#' shinyApp(app$ui, app$server)
+#' }
+#'
+#'
 tm_t_smq <- function(label,
                      dataname,
                      parentname = ifelse(is(arm_var, "data_extract_spec"), datanames_input(arm_var), "ADSL"),
@@ -276,38 +356,28 @@ tm_t_smq <- function(label,
                      na_level = "<Missing>",
                      smq_varlabel = "Standardized MedDRA Query",
                      baskets,
+                     scopes,
                      keys = c("STUDYID", "USUBJID", "ASTDTM", "AESEQ", "AETERM"),
                      sort_by_descending = TRUE,
                      pre_output = NULL,
                      post_output = NULL) {
-  # stop_if_not(
-  #   is.string(dataname),
-  #   is.choices_selected(arm_var),
-  #   is.flag(add_total),
-  #   is.choices_selected(by_vars),
-  #   is.choices_selected(grade),
-  #   is_character_vector(abnormal),
-  #   is.choices_selected(id_var),
-  #   is.choices_selected(baseline_var),
-  #   is.choices_selected(treatment_flag),
-  #   is.choices_selected(treatment_flag_var),
-  #   is_logical_single(exclude_base_abn),
-  #   is.flag(drop_arm_levels),
-  #   list(
-  #     is.null(pre_output) || is(pre_output, "shiny.tag"),
-  #     "pre_output should be either null or shiny.tag type of object"
-  #   ),
-  #   list(
-  #     is.null(post_output) || is(post_output, "shiny.tag"),
-  #     "post_output should be either null or shiny.tag type of object"
-  #   )
-  # )
+  stop_if_not(
+    list(
+      is.null(pre_output) || is(pre_output, "shiny.tag"),
+      "pre_output should be either null or shiny.tag type of object"
+    ),
+    list(
+      is.null(post_output) || is(post_output, "shiny.tag"),
+      "post_output should be either null or shiny.tag type of object"
+    )
+  )
 
   data_extract_list <- list(
     arm_var = cs_to_des_select(arm_var, dataname = parentname),
     id_var = cs_to_des_select(id_var, dataname = dataname),
     col_by_var = cs_to_des_select(col_by_var, dataname = parentname),
     baskets = cs_to_des_select(baskets, dataname = dataname),
+    scopes = cs_to_des_select(scopes, dataname = dataname),
     llt = cs_to_des_select(llt, dataname = dataname),
   )
 
@@ -315,19 +385,195 @@ tm_t_smq <- function(label,
 
   module(
     label = label,
-    ui = ui_t_abnormality,
-    server = srv_t_abnormality,
+    ui = ui_t_smq,
+    server = srv_t_smq,
     ui_args = c(data_extract_list, args),
     server_args = c(
       data_extract_list,
       list(
         dataname = dataname,
         parentname = parentname,
-        abnormal = abnormal,
+        na_level = na_level,
         label = label
       )
     ),
     filters = get_extract_datanames(data_extract_list)
   )
 }
+
+#' @noRd
+ui_t_exposure <- function(id, ...) {
+
+  ns <- NS(id)
+  a <- list(...) # module args
+
+  is_single_dataset_value <- is_single_dataset(
+    a$arm_var,
+    a$col_by_var,
+    a$id_var,
+    a$baskets,
+    a$scopes,
+    a$llt
+  )
+
+  standard_layout(
+    output = white_small_well(table_with_settings_ui(ns("table"))),
+    encoding = div(
+      tags$label("Encodings", class = "text-primary"),
+      datanames_input(a[c(
+        "arm_var", "col_by_var", "id_var", "baskets", "llt"
+      )]),
+      data_extract_input(
+        id = ns("arm_var"),
+        label = "Select Treatment Variable",
+        data_extract_spec = a$arm_var,
+        is_single_dataset = is_single_dataset_value
+      ),
+      checkboxInput(ns("add_total"), "Add All Patients column", value = a$add_total),
+      data_extract_input(
+        id = ns("col_by_var"),
+        label = "Select additional column by variable, if desired",
+        data_extract_spec = a$arm_var,
+        is_single_dataset = is_single_dataset_value
+      ),
+      data_extract_input(
+        id = ns("baskets"),
+        label = "Select the SMQXXNAM/CQXXNAM baskets",
+        data_extract_spec = a$arm_var,
+        is_single_dataset = is_single_dataset_value,
+        multiple = TRUE
+      ),
+      data_extract_input(
+        id = ns("scopes"),
+        label = "Scope variables available",
+        data_extract_spec = a$scopes,
+        is_single_dataset = is_single_dataset_value,
+        multiple = TRUE
+      ),
+      data_extract_input(
+        id = ns("llt"),
+        label = "Select the low level term",
+        data_extract_spec = a$arm_var,
+        is_single_dataset = is_single_dataset_value
+      ),
+      checkboxInput(ns("sort_by_descending"), "Add All Patients column", value = a$sort_by_descending),
+      checkboxInput(ns("add_total"), "Add All Patients column", value = a$add_total),
+      panel_group(
+        panel_item(
+          "Additional Variables Info",
+          data_extract_input(
+            id = ns("id_var"),
+            label = "Subject Identifier",
+            data_extract_spec = a$id_var,
+            is_single_dataset = is_single_dataset_value
+          )
+        )
+      )
+    ),
+    forms = get_rcode_ui(ns("rcode")),
+    pre_output = a$pre_output,
+    post_output = a$post_output
+  )
+}
+
+srv_t_exposure <- function(input,
+                           output,
+                           session,
+                           datasets,
+                           dataname,
+                           parentname,
+                           arm_var,
+                           id_var,
+                           col_by_var,
+                           baskets,
+                           scopes,
+                           llt,
+                           na_level,
+                           label) {
+  stopifnot(is_cdisc_data(datasets))
+
+  init_chunks()
+
+  anl_merged <- data_merge_module(
+    datasets = datasets,
+    data_extract = list(arm_var, id_var, col_by_var, baskets, scopes, llt),
+    input_id = c("id_var", "col_by_var", "baskets", "scopes", "llt"),
+    merge_function = "dplyr::inner_join"
+  )
+
+  adsl_merged <- data_merge_module(
+    datasets = datasets,
+    data_extract = list(col_by_var, arm_var),
+    input_id = c("col_by_var", "arm_var"),
+    anl_name = "ANL_ADSL"
+  )
+
+  validate_checks <- reactive({
+    adsl_filtered <- datasets$get_data(parentname, filtered = TRUE)
+    anl_filtered <- datasets$get_data(dataname, filtered = TRUE)
+    anl_m <- anl_merged()
+    anl_adsl <- adsl_merged()
+
+    input_arm_var <- as.vector(anl_m$columns_source$arm_var)
+    input_col_by_var <- as.vector(anl_adsl$columns_source$col_by_var)
+    input_id_var <- as.vector(anl_m$columns_source$id_var)
+    input_baskets <- as.vector(anl_m$columns_source$baskets)
+    input_scopes <- as.vector(anl_m$columns_source$scopes)
+    input_llt <- as.vector(anl_m$columns_source$llt)
+  })
+
+  call_preparation <- reactive({
+    validate_checks()
+
+    chunks_reset()
+    anl_m <- anl_merged()
+    chunks_push_data_merge(anl_m)
+    chunks_push_new_line()
+    anl_adsl <- adsl_merged()
+    chunks_push_data_merge(anl_adsl)
+    chunks_push_new_line()
+
+    my_calls <- template_smq(
+      parentname = "ANL_ADSL",
+      dataname = "ANL",
+      arm_var = as.vector(anl_m$columns_source$arm_var),
+      id_var = as.vector(anl_m$columns_source$id_var),
+      col_by_var = as.vector(anl_adsl$columns_source$col_by_var),
+      baskets = as.vector(anl_m$columns_source$baskets),
+      scopes = as.vector(anl_m$columns_source$scopes),
+      llt = as.vector(anl_m$columns_source$llt),
+      sort_by_descending = input$sort_by_descending,
+      add_total = input$add_total,
+      drop_arm_levels = input$drop_arm_levels,
+      na_level = na_level
+    )
+    mapply(expression = my_calls, chunks_push)
+  })
+
+  # Outputs to render.
+  table <- reactive({
+    call_preparation()
+    chunks_safe_eval()
+    chunks_get_var("result")
+  })
+
+  callModule(
+    table_with_settings_srv,
+    id = "table",
+    table_r = table
+  )
+
+  # Render R code.
+  callModule(
+    module = get_rcode_srv,
+    id = "rcode",
+    datasets = datasets,
+    datanames = get_extract_datanames(
+      list(arm_var, id_var, col_by_var, baskets, scopes, llt)
+    ),
+    modal_title = "R Code for SMQ tables",
+    code_header = label
+  )
+}
+
 
