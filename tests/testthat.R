58dbea02bfe39dc8b4fb5c9632ebaa4a672ce04b
Dinakar <26552821+dinakar29@users.noreply.github.com>
2021-09-24 16:52:59 -0500

    Enhance CI workflows (#127)

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,10 @@
-library(testthat)
-
-test_results <- test_check("teal.modules.clinical")
-saveRDS(test_results, "unit_testing_results.rds")
+pkg_name <- "teal.modules.clinical"
+if (requireNamespace("testthat", quietly = TRUE)) {
+  library(testthat)
+  reporter <- MultiReporter$new(list(
+    CheckReporter$new(),
+    JunitReporter$new(file = "junit-result.xml")
+  ))
+  test_results <- test_check(pkg_name, reporter = reporter)
+  saveRDS(test_results, "unit_testing_results.rds")
+}
