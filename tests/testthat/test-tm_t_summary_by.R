c1aec53931b5c22ff7ba30b2e8ef83d3a86210cb
Rosemary Li <liy396@gene.com>
2021-09-30 10:54:51 -0700

    update unit test

--- tests/testthat/test-tm_t_summary_by.R
+++ tests/testthat/test-tm_t_summary_by.R
@@ -283,7 +283,13 @@ test_that("template_summary_by generates correct expressions for `drop_zero_leve
         )
     ),
     table = quote({
-      result <- build_table(lyt = lyt, df = anl, alt_counts_df = adsl) %>% trim_rows()
+      result <- build_table(lyt = lyt, df = anl, alt_counts_df = adsl) %>%
+        trim_rows(criteria = function(tr) {
+          if (!is(tr, "TableRow") || is(tr, "LabelRow"))
+            return(FALSE)
+          rvs = unlist(unname(row_values(tr)))
+          all(rvs == 0)
+        })
       result
     })
   )
