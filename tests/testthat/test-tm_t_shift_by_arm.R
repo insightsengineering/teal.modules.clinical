f3a84d51fddf1733e93ec019e74e6314cffe4325
Rosemary Li <42564519+yli110-stat697@users.noreply.github.com>
2021-09-30 10:21:32 -0700

    133 add use na in tm t shift by arm (#143)
    * add na.rm in template fun
    
    * add useNA in module
    
    * doc update
    
    * update unit test

--- tests/testthat/test-tm_t_shift_by_arm.R
+++ tests/testthat/test-tm_t_shift_by_arm.R
@@ -82,3 +82,46 @@ test_that("template_shift_by_arm generates correct expressions with add_total be
   )
   expect_equal(result, expected)
 })
+
+test_that("template_shift_by_arm generates correct expressions with na.rm being TRUE", {
+  result <- template_shift_by_arm(
+    parentname = "adsl",
+    dataname = "adeg",
+    arm_var = "ARM",
+    paramcd = "PARAMCD",
+    visit_var = "AVISIT",
+    aval_var = "ANRIND",
+    base_var = "BNRIND",
+    na.rm = TRUE,
+    na_level = "<Missing>"
+  )
+
+  expected <- list(
+    data = quote({
+      adsl <- df_explicit_na(adsl, na_level = "<Missing>")
+      adeg <- df_explicit_na(adeg, na_level = "<Missing>") %>%
+        filter(ONTRTFL == "Y")
+      attr(adeg$BNRIND, "label") <- "Baseline Assessment"
+    }),
+
+    layout = quote(
+      lyt <- basic_table() %>%
+        split_cols_by("AVISIT", split_fun = drop_split_levels) %>%
+        split_cols_by("ANRIND") %>%
+        split_rows_by(
+          "ARM",
+          split_fun = drop_split_levels,
+          label_pos = "topleft",
+          split_label = obj_label(adeg$ARM)
+        ) %>%
+        add_rowcounts() %>%
+        summarize_vars("BNRIND", denom = "N_row", na_level = "<Missing>", na.rm = TRUE, .stats = "count_fraction") %>%
+        append_varlabels(adeg, "BNRIND", indent = 1L)
+    ),
+    table = quote({
+      result <- build_table(lyt = lyt, df = adeg)
+      result
+    })
+  )
+  expect_equal(result, expected)
+})
