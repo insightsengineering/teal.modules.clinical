6da0063b79845c92f1c8de7479dc8c3c76104197
Junlue Zhao <jzhao31415@gmail.com>
2021-12-16 09:02:35 -0600

    ASTDY for cmstdy parameter in prior_medication

--- R/tm_t_pp_prior_medication.R
+++ R/tm_t_pp_prior_medication.R
@@ -12,7 +12,7 @@ template_prior_medication <- function(dataname = "ANL",
                                       atirel = "ATIREL",
                                       cmdecod = "CMDECOD",
                                       cmindc = "CMINDC",
-                                      cmstdy = "CMSTDY") {
+                                      cmstdy = "ASTDY") {
   assert_that(
     is.string(dataname),
     is.string(atirel),
@@ -82,21 +82,14 @@ template_prior_medication <- function(dataname = "ANL",
 #' ADCM$CMDOSU <- "U"
 #' ADCM$CMROUTE <- "CMROUTE"
 #' ADCM$CMDOSFRQ <- "CMDOSFRQ"
-#' ADCM$CMSTDY <- 1
-#' ADCM[ADCM$CMCAT == "medcl B", ]$CMSTDY <- 20
-#' ADCM[ADCM$CMCAT == "medcl C", ]$CMSTDY <- 150
-#' ADCM$CMENDY <- 500
-#' ADCM[ADCM$CMCAT == "medcl B", ]$CMENDY <- 700
-#' ADCM[ADCM$CMCAT == "medcl C", ]$CMENDY <- 1000
 #' ADCM$CMASTDTM <- ADCM$ASTDTM
 #' ADCM$CMAENDTM <- ADCM$AENDTM
 #' rtables::var_labels(
-#'   ADCM[c("CMINDC", "CMTRT", "CMSTDY", "CMENDY")]
+#'   ADCM[c("CMINDC", "CMTRT", "ASTDY")]
 #' ) <- c(
 #'   "Indication",
 #'   "Reported Name of Drug, Med, or Therapy",
-#'   "Study Day of Start of Medication",
-#'   "Study Day of End of Medication"
+#'   "Study Day of Start of Medication"
 #' )
 #' adcm_keys <- c("STUDYID", "USUBJID", "ASTDTM", "CMSEQ", "ATC1", "ATC2", "ATC3", "ATC4")
 #'
@@ -111,21 +104,15 @@ template_prior_medication <- function(dataname = "ANL",
 #'       ADCM$CMDOSU <- "U"
 #'       ADCM$CMROUTE <- "CMROUTE"
 #'       ADCM$CMDOSFRQ <- "CMDOSFRQ"
-#'       ADCM$CMSTDY <- 1
-#'       ADCM[ADCM$CMCAT == "medcl B", ]$CMSTDY <- 20
-#'       ADCM[ADCM$CMCAT == "medcl C", ]$CMSTDY <- 150
-#'       ADCM$CMENDY <- 500
-#'       ADCM[ADCM$CMCAT == "medcl B", ]$CMENDY <- 700
-#'       ADCM[ADCM$CMCAT == "medcl C", ]$CMENDY <- 1000
 #'       ADCM$CMASTDTM <- ADCM$ASTDTM
 #'       ADCM$CMAENDTM <- ADCM$AENDTM
 #'       rtables::var_labels(
-#'         ADCM[c("CMINDC", "CMTRT", "CMSTDY", "CMENDY")]) <- c(
+#'         ADCM[c("CMINDC", "CMTRT", "ASTDY")]) <- c(
 #'           "Indication",
 #'           "Reported Name of Drug, Med, or Therapy",
-#'           "Study Day of Start of Medication",
-#'           "Study Day of End of Medication")',
-#'                   keys = adcm_keys
+#'           "Study Day of Start of Medication"
+#'          )',
+#'     keys = adcm_keys
 #'     ),
 #'     check = TRUE
 #'   ),
@@ -148,8 +135,8 @@ template_prior_medication <- function(dataname = "ANL",
 #'         selected = "CMINDC"
 #'       ),
 #'       cmstdy = choices_selected(
-#'         choices = variable_choices(ADCM, "CMSTDY"),
-#'         selected = "CMSTDY"
+#'         choices = variable_choices(ADCM, "ASTDY"),
+#'         selected = "ASTDY"
 #'       )
 #'     )
 #'   )
