767fb3e1f67af52c55441c4182babb20ebeae048
Jun Dao <jzhao31415@gmail.com>
2021-12-17 09:14:59 -0600

    Changed roxygen sample APP and default arguments (#265)
    * Changed roxygen sample APP and default arguments
    
    * removed as.numeric conversion
    
    * ASTDY for cmstdy parameter in prior_medication
    
    * Reverted to correct default arguments
    
    * roxygen
    
    Co-authored-by: mhallal1 <86970066+mhallal1@users.noreply.github.com>
    Co-authored-by: Mahmoud Hallal <mahmoud.hallal@roche.com>

--- R/tm_g_pp_therapy.R
+++ R/tm_g_pp_therapy.R
@@ -240,6 +240,7 @@ template_therapy <- function(dataname = "ANL",
 #'
 #' @examples
 #' library(scda)
+#'
 #' ADSL <- synthetic_cdisc_data("latest")$adsl
 #' ADCM <- synthetic_cdisc_data("latest")$adcm
 #'
@@ -250,16 +251,10 @@ template_therapy <- function(dataname = "ANL",
 #' ADCM$CMDOSU <- "U"
 #' ADCM$CMROUTE <- "CMROUTE"
 #' ADCM$CMDOSFRQ <- "CMDOSFRQ"
-#' ADCM$CMSTDY <- 1
-#' ADCM[ADCM$CMCAT == "medcl B", ]$CMSTDY <- 20
-#' ADCM[ADCM$CMCAT == "medcl C", ]$CMSTDY <- 150
-#' ADCM$CMENDY <- 500
-#' ADCM[ADCM$CMCAT == "medcl B", ]$CMENDY <- 700
-#' ADCM[ADCM$CMCAT == "medcl C", ]$CMENDY <- 1000
 #' ADCM$CMASTDTM <- ADCM$ASTDTM
 #' ADCM$CMAENDTM <- ADCM$AENDTM
 #' rtables::var_labels(
-#'   ADCM[c("CMINDC", "CMTRT", "CMSTDY", "CMENDY")]
+#'   ADCM[c("CMINDC", "CMTRT", "ASTDY", "AENDY")]
 #' ) <- c(
 #'   "Indication",
 #'   "Reported Name of Drug, Med, or Therapy",
@@ -279,16 +274,10 @@ template_therapy <- function(dataname = "ANL",
 #'       ADCM$CMDOSU <- "U"
 #'       ADCM$CMROUTE <- "CMROUTE"
 #'       ADCM$CMDOSFRQ <- "CMDOSFRQ"
-#'       ADCM$CMSTDY <- 1
-#'       ADCM[ADCM$CMCAT == "medcl B", ]$CMSTDY <- 20
-#'       ADCM[ADCM$CMCAT == "medcl C", ]$CMSTDY <- 150
-#'       ADCM$CMENDY <- 500
-#'       ADCM[ADCM$CMCAT == "medcl B", ]$CMENDY <- 700
-#'       ADCM[ADCM$CMCAT == "medcl C", ]$CMENDY <- 1000
 #'       ADCM$CMASTDTM <- ADCM$ASTDTM
 #'       ADCM$CMAENDTM <- ADCM$AENDTM
 #'       rtables::var_labels(
-#'         ADCM[c("CMINDC", "CMTRT", "CMSTDY", "CMENDY")]) <- c(
+#'         ADCM[c("CMINDC", "CMTRT", "ASTDY", "AENDY")]) <- c(
 #'           "Indication",
 #'           "Reported Name of Drug, Med, or Therapy",
 #'           "Study Day of Start of Medication",
@@ -337,16 +326,17 @@ template_therapy <- function(dataname = "ANL",
 #'         selected = "CMDOSFRQ"
 #'       ),
 #'       cmstdy = choices_selected(
-#'         choices = variable_choices(ADCM, "CMSTDY"),
-#'         selected = "CMSTDY"
+#'         choices = variable_choices(ADCM, "ASTDY"),
+#'         selected = "ASTDY"
 #'       ),
 #'       cmendy = choices_selected(
-#'         choices = variable_choices(ADCM, "CMENDY"),
-#'         selected = "CMENDY"
+#'         choices = variable_choices(ADCM, "AENDY"),
+#'         selected = "AENDY"
 #'       )
 #'     )
 #'   )
 #' )
+#'
 #' \dontrun{
 #' shinyApp(app$ui, app$server)
 #' }
