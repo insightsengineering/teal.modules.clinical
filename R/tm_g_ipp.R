506745561c8b47c85437f189ada07de21d1e1456
Maciej Nasinski <nasinski.maciej@gmail.com>
2022-01-26 14:41:16 +0100

    ggplot args docs fix

--- R/tm_g_ipp.R
+++ R/tm_g_ipp.R
@@ -171,6 +171,12 @@ template_g_ipp <- function(dataname = "ANL",
 #' @param base_var ([teal::choices_selected()] or [teal::data_extract_spec()])\cr
 #'   object with all available choices
 #'   and preselected option for variable values that can be used as `base_var`.
+#' @param ggplot2_args optional, (`ggplot2_args`)\cr
+#' object created by [teal.devel::ggplot2_args()] with settings for the module plot.
+#' For this module, this argument will only accept `labs` arguments such as: `title`, `subtitle`, `x`, `y`.
+#' `theme` arguments will be not taken into account. The argument is merged with option `teal.ggplot2_args` and
+#' with default module arguments (hard coded in the module body).\cr For more details, see the help vignette:\cr
+#' `vignette("custom-ggplot2-arguments", package = "teal.devel")`.
 #'
 #' @export
 #'
