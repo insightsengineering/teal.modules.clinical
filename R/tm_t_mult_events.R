cac2950564363598d3b59ec8c06add8ffdca0a78
brandondbutcher <butcherb@gene.com>
2022-02-09 13:43:23 -0800

    Merge main updates into branch.

--- R/tm_t_mult_events.R
+++ R/tm_t_mult_events.R
@@ -521,8 +521,8 @@ srv_t_mult_events_byterm <- function(input,
     input_hlt <- anl_selectors()$hlt()$select_ordered
     input_llt <- as.vector(anl_m$columns_source$llt)
 
-    hlt_labels <- mapply(function(x) rtables::obj_label(ADCM[[x]]), input_hlt)
-    llt_labels <- mapply(function(x) rtables::obj_label(ADCM[[x]]), input_llt)
+    hlt_labels <- mapply(function(x) rtables::obj_label(anl_adsl[[x]]), input_hlt)
+    llt_labels <- mapply(function(x) rtables::obj_label(anl_adsl[[x]]), input_llt)
 
     basic_table_args$title <- ifelse(
       is.null(basic_table_args$title),
