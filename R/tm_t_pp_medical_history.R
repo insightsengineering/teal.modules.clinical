3c23ac7df7c7f2e4284808e61667162b0ea956ad
Dawid Kałędkowski <dawid.kaledkowski@gmail.com>
2022-03-15 22:46:32 +0100

    lintr/styler

--- R/tm_t_pp_medical_history.R
+++ R/tm_t_pp_medical_history.R
@@ -223,7 +223,10 @@ srv_t_medical_history <- function(id,
 
     # Init
     patient_data_base <- reactive(unique(datasets$get_data(parentname, filtered = TRUE)[[patient_col]]))
-    teal.widgets::updateOptionalSelectInput(session, "patient_id", choices = patient_data_base(), selected = patient_data_base()[1])
+    teal.widgets::updateOptionalSelectInput(
+      session, "patient_id",
+      choices = patient_data_base(), selected = patient_data_base()[1]
+    )
 
     observeEvent(patient_data_base(),
       handlerExpr = {
