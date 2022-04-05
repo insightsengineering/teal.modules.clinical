045fdc7eddf484d818fec5bdd214b789bbdde613
Rosemary Li <liy396@gene.com>
2022-04-04 23:25:18 -0700

    double quoted to fix super lintr

--- R/tm_t_binary_outcome.R
+++ R/tm_t_binary_outcome.R
@@ -907,7 +907,7 @@ srv_t_binary_outcome <- function(id,
         combine_comp_arms = input$combine_comp_arms,
         aval_var = input_aval_var,
         responder_val = input$responders,
-        responder_val_levels = default_responses[[input_paramcd]][['levels']],
+        responder_val_levels = default_responses[[input_paramcd]][["levels"]],
         show_rsp_cat = input$show_rsp_cat,
         control = list(
           global = list(
