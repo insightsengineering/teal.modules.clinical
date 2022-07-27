66f54cd56f8dcb62c84021cdee95d7df0b96ddcb
Mahmoud Hallal <86970066+mhallal1@users.noreply.github.com>
2022-07-27 11:13:23 +0200

    reporter cleaning (#537)
    * remove titles
    
    * simple_reporter
    
    * remove space
    
    * Update R/tm_a_mmrm.R
    
    Co-authored-by: Nikolas Burkoff <nikolas.burkoff@roche.com>

--- R/tm_t_abnormality_by_worst_grade.R
+++ R/tm_t_abnormality_by_worst_grade.R
@@ -378,12 +378,7 @@ ui_t_abnormality_by_worst_grade <- function(id, ...) { # nolint
     output = teal.widgets::white_small_well(teal.widgets::table_with_settings_ui(ns("table"))),
     encoding = shiny::div(
       ### Reporter
-      shiny::tags$div(
-        teal.reporter::add_card_button_ui(ns("addReportCard")),
-        teal.reporter::download_report_button_ui(ns("downloadButton")),
-        teal.reporter::reset_report_button_ui(ns("resetButton"))
-      ),
-      shiny::tags$br(),
+      teal.reporter::simple_reporter_ui(ns("simple_reporter")),
       ###
       shiny::tags$label("Encodings", class = "text-primary"),
       teal.transform::datanames_input(
@@ -601,7 +596,6 @@ srv_t_abnormality_by_worst_grade <- function(id, # nolint
         card$set_name("Laboratory Test Results Table")
         card$append_text("Laboratory Test Results Table", "header2")
         card$append_text("Laboratory test results with highest grade post-baseline Table", "header3")
-        card$append_text("Filter State", "header3")
         card$append_fs(datasets$get_filter_state())
         card$append_text("Table", "header3")
         card$append_table(table_r())
@@ -609,19 +603,15 @@ srv_t_abnormality_by_worst_grade <- function(id, # nolint
           card$append_text("Comment", "header3")
           card$append_text(comment)
         }
-        card$append_text("Show R Code", "header3")
         card$append_src(paste(get_rcode(
-          chunks = teal.code::get_chunks_object(parent_idx = 1L),
+          chunks = teal.code::get_chunks_object(parent_idx = 2L),
           datasets = datasets,
           title = "",
           description = ""
         ), collapse = "\n"))
         card
       }
-
-      teal.reporter::add_card_button_srv("addReportCard", reporter = reporter, card_fun = card_fun)
-      teal.reporter::download_report_button_srv("downloadButton", reporter = reporter)
-      teal.reporter::reset_report_button_srv("resetButton", reporter)
+      teal.reporter::simple_reporter_srv("simple_reporter", reporter = reporter, card_fun = card_fun)
     }
     ###
   })
