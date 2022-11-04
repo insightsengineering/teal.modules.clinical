4c9531fad398dc6e5f1a896b438d222119beebf6
Dawid Kałędkowski <dawid.kaledkowski@gmail.com>
2022-11-04 16:05:59 +0100

    Fix styler

--- R/tm_t_coxreg.R
+++ R/tm_t_coxreg.R
@@ -1047,8 +1047,8 @@ srv_t_coxreg <- function(id,
               function(x) {
                 call_template(x, merged$anl_input_r(), paramcd, multivariate, all_basic_table_args)
               }
-            ))
-          )) %>%
+            )))
+          ) %>%
           teal.code::eval_code(
             substitute(
               expr = {
