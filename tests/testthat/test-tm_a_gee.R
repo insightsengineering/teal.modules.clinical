bed1691509885250585464b2b2885d6188dfed92
Emily de la Rua <emily.de_la_rua@contractors.roche.com>
2022-11-24 16:28:16 -0500

    Add unit test

--- tests/testthat/test-tm_a_gee.R
+++ tests/testthat/test-tm_a_gee.R
@@ -1,4 +1,4 @@
-testthat::test_that("template_a_gee works as expected with default input", {
+testthat::test_that("template_a_gee t_gee_cov table works as expected with default input", {
   result <- template_a_gee(
     output_table = "t_gee_cov",
     aval_var = "AVAL",
@@ -25,6 +25,38 @@ testthat::test_that("template_a_gee works as expected with default input", {
     }),
     table = quote({
       result_table <- tern.gee::as.rtable(model_fit, type = "cov")
+    })
+  )
+  testthat::expect_equal(result, expected)
+})
+
+testthat::test_that("template_a_gee t_gee_coef table works as expected with default input", {
+  result <- template_a_gee(
+    output_table = "t_gee_coef",
+    aval_var = "AVAL",
+    id_var = "USUBJID",
+    arm_var = "ARMCD",
+    visit_var = "AVISIT",
+    split_covariates = c(),
+    cor_struct = "unstructured"
+  )
+  expected <- list(
+    model = quote({
+      model_fit <- tern.gee::fit_gee(
+        vars = tern.gee::vars_gee(
+          response = as.vector("AVAL"),
+          covariates = as.vector(NULL),
+          id = as.vector("USUBJID"),
+          arm = as.vector("ARMCD"),
+          visit = as.vector("AVISIT")
+        ),
+        data = ANL,
+        regression = "logistic",
+        cor_struct = "unstructured"
+      )
+    }),
+    table = quote({
+      result_table <- tern.gee::as.rtable(model_fit, type = "coef", conf_level = 0.95)
     })
   )
   testthat::expect_equal(result, expected)
