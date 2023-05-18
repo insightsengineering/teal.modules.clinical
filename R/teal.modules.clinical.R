8a1a90609bf4b85c8238c01fdedd1e677cbeedd7
Emily de la Rua <emily.de_la_rua@contractors.roche.com>
2023-05-18 10:53:45 -0400

    Add global variable definitions

--- R/teal.modules.clinical.R
+++ R/teal.modules.clinical.R
@@ -17,9 +17,11 @@ NULL
 
 # We need this to avoid R CMD check warning about missing global definitions.
 utils::globalVariables(c(
+  "arm_var",
   "column_name",
   "n_column_name",
   "new_arm_ref_comp",
   "new_comp_arm",
-  "usubjid"
+  "usubjid",
+  "x_var"
 ))
