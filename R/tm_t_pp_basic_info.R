5d38168e2470d1816f29dff53c68d2777376b878
Aleksander Chlebowski <aleksander.chlebowski@contractors.roche.com>
2023-08-07 12:15:33 +0200

    change pagination options

--- R/tm_t_pp_basic_info.R
+++ R/tm_t_pp_basic_info.R
@@ -119,7 +119,6 @@ ui_t_basic_info <- function(id, ...) {
   ns <- shiny::NS(id)
   teal.widgets::standard_layout(
     output = shiny::div(
-      teal.widgets::get_dt_rows(ns("basic_info_table"), ns("basic_info_table_rows")),
       DT::DTOutput(outputId = ns("basic_info_table"))
     ),
     encoding = shiny::div(
@@ -243,7 +242,9 @@ srv_t_basic_info <- function(id,
 
     output$basic_info_table <- DT::renderDataTable(
       expr = table_r(),
-      options = list(pageLength = input$basic_info_table_rows)
+      options = list(
+        lengthMenu = list(list(-1, 5, 10, 25), list("All", "5", "10", "25"))
+      )
     )
 
     teal.widgets::verbatim_popup_srv(
