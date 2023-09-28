f34e342eaf1ed5cf2d32420edba4fb94f4177655
André Veríssimo <211358+averissimo@users.noreply.github.com>
2023-09-28 18:03:39 +0200

    feat: use card template to remove repeated code

--- R/tm_t_shift_by_arm.R
+++ R/tm_t_shift_by_arm.R
@@ -529,14 +529,12 @@ srv_shift_by_arm <- function(id,
     ### REPORTER
     if (with_reporter) {
       card_fun <- function(comment, label) {
-        card <- teal::TealReportCard$new()
-        title <- "Shift by Arm Table"
-        title <- ifelse(label == "", title, label)
-        card$set_name(title)
-        card$append_text(title, "header2")
-        if (with_filter) {
-          card$append_fs(filter_panel_api$get_filter_state())
-        }
+        card <- card_template(
+          title = "Shift by Arm Table",
+          label = label,
+          description = NULL,
+          with_filter = with_filter
+        )
         card$append_text("Table", "header3")
         card$append_table(table_r())
         if (!comment == "") {
