d6952943ff5157a1bfb2072fc104a9cdb3fc4c00
kartikeya kirar <kirar.kartikeya1@gmail.com>
2023-11-23 16:28:31 +0530

    `tdata` to `teal_data` - `tm_t_pp_basic_info` (#869)
    **Example App**
    
    ```
    ADSL <- tmc_ex_adsl
    
    app <- init(
      data = cdisc_data(
        ADSL = ADSL,
        code = "ADSL <- tmc_ex_adsl"
      ),
      modules = modules(
        tm_t_pp_basic_info(
          label = "Basic Info",
          dataname = "ADSL",
          patient_col = "USUBJID",
          vars = choices_selected(
            choices = variable_choices(ADSL),
            selected = c("ARM", "AGE", "SEX", "COUNTRY", "RACE", "EOSSTT")
          )
        )
      )
    )
    shinyApp(app$ui, app$server)
    ```

--- R/tm_t_pp_basic_info.R
+++ R/tm_t_pp_basic_info.R
@@ -172,13 +172,14 @@ srv_t_basic_info <- function(id,
                              label) {
   with_reporter <- !missing(reporter) && inherits(reporter, "Reporter")
   with_filter <- !missing(filter_panel_api) && inherits(filter_panel_api, "FilterPanelAPI")
-  checkmate::assert_class(data, "tdata")
+  checkmate::assert_class(data, "reactive")
+  checkmate::assert_class(isolate(data()), "teal_data")
 
   shiny::moduleServer(id, function(input, output, session) {
     patient_id <- shiny::reactive(input$patient_id)
 
     # Init
-    patient_data_base <- shiny::reactive(unique(data[[dataname]]()[[patient_col]]))
+    patient_data_base <- shiny::reactive(unique(data()[[dataname]][[patient_col]]))
     teal.widgets::updateOptionalSelectInput(
       session,
       "patient_id",
@@ -219,13 +220,12 @@ srv_t_basic_info <- function(id,
 
     anl_inputs <- teal.transform::merge_expression_srv(
       datasets = data,
-      join_keys = teal.data::join_keys(data),
       selector_list = selector_list,
       merge_function = "dplyr::left_join"
     )
 
     anl_q <- shiny::reactive({
-      teal.code::new_qenv(tdata2env(data), code = get_code_tdata(data)) %>%
+      data() %>%
         teal.code::eval_code(as.expression(anl_inputs()$expr))
     })
 
