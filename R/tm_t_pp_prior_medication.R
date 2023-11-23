38fd50098661a2a54fc1464d8bb19267a0972939
unknown <kirar.kartikeya1@gmail.com>
2023-11-23 12:11:14 +0530

    tdata to teal_data - tm_t_pp_prior_medication

--- R/tm_t_pp_prior_medication.R
+++ R/tm_t_pp_prior_medication.R
@@ -241,7 +241,8 @@ srv_t_prior_medication <- function(id,
                                    label) {
   with_reporter <- !missing(reporter) && inherits(reporter, "Reporter")
   with_filter <- !missing(filter_panel_api) && inherits(filter_panel_api, "FilterPanelAPI")
-  checkmate::assert_class(data, "tdata")
+  checkmate::assert_class(data, "reactive")
+  checkmate::assert_class(isolate(data()), "teal_data")
 
   shiny::moduleServer(id, function(input, output, session) {
     patient_id <- shiny::reactive(input$patient_id)
@@ -269,7 +270,7 @@ srv_t_prior_medication <- function(id,
     })
 
     # Init
-    patient_data_base <- shiny::reactive(unique(data[[parentname]]()[[patient_col]]))
+    patient_data_base <- shiny::reactive(unique(data()[[parentname]][[patient_col]]))
     teal.widgets::updateOptionalSelectInput(
       session,
       "patient_id",
@@ -297,12 +298,11 @@ srv_t_prior_medication <- function(id,
     anl_inputs <- teal.transform::merge_expression_srv(
       datasets = data,
       selector_list = selector_list,
-      join_keys = teal.data::join_keys(data),
       merge_function = "dplyr::left_join"
     )
 
     anl_q <- shiny::reactive({
-      teal.code::new_qenv(tdata2env(data), code = get_code_tdata(data)) %>%
+      data() %>%
         teal.code::eval_code(as.expression(anl_inputs()$expr))
     })
 
@@ -317,8 +317,9 @@ srv_t_prior_medication <- function(id,
         cmstdy = input[[extract_input("cmstdy", dataname)]]
       )
 
-      teal.code::eval_code(
-        anl_q(),
+
+      anl_q() %>%
+        teal.code::eval_code(
         substitute(
           expr = {
             ANL <- ANL[ANL[[patient_col]] == patient_id, ] # nolint
