7edbd37a5cd7f8be88240d577afa9eb3ba4932aa
Pawel Rucki <12943682+pawelru@users.noreply.github.com>
2024-01-26 17:49:41 +0100

    temp silent certain warning

--- tests/testthat/test-examples.R
+++ tests/testthat/test-examples.R
@@ -1,14 +1,28 @@
 files <- devtools:::rd_files()
 
+# this is temporary
+# every app tested with shinytest2 throws a warning because of https://github.com/insightsengineering/teal.code/issues/194
+# silent certain warnings
+suppress_warnings <- function(expr, pattern, ...) {
+  withCallingHandlers(expr, warning = function(w) {
+    if (grepl(pattern, conditionMessage(w))) {
+      invokeRestart("muffleWarning")
+    }
+  })
+}
+
 for (i in files) {
   with_mocked_bindings(
     test_that(
-        paste0("example-", i),{
-            skip_on_cran()
-            expect_no_error(
-                pkgload::run_example(i, run_donttest = TRUE, run_dontrun = FALSE)
-            )
-        }
+      paste0("example-", basename(i)),{
+        skip_on_cran()
+        expect_no_error(
+          suppress_warnings( # temporary (see above)
+            pkgload::run_example(i, run_donttest = TRUE, run_dontrun = FALSE, quiet = TRUE),
+            "may not be available when loading"
+          )
+        )
+      }
     ),
     runApp = \(appDir, ...) {
       cat("hello from mocked runApp\n")
