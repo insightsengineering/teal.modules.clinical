4b4849d2f50b550447ff95e8b439f5003499eb46
kartikeya kirar <kirar.kartikeya1@gmail.com>
2024-02-14 10:02:22 +0530

    removing internal functions example. (#1051)
    fixes:
    https://github.com/insightsengineering/teal.modules.clinical/issues/1050
    
    ---------
    
    Co-authored-by: Marcin <133694481+m7pr@users.noreply.github.com>

--- R/arm_ref_comp.R
+++ R/arm_ref_comp.R
@@ -24,56 +24,6 @@
 #'   and comparison arm
 #' @keywords internal
 #'
-#' @examples
-#' library(teal.widgets)
-#' library(shinyvalidate)
-#'
-#' arm_ref_comp_observer <- getFromNamespace("arm_ref_comp_observer", "teal.modules.clinical")
-#'
-#' arm_ref_comp <- list(ARMCD = list(ref = "ARM A", comp = c("ARM B")))
-#' arm_var <- choices_selected(c("ARM", "ARMCD"), "ARMCD")
-#'
-#' adsl <- data.frame(ARM = c("ARM 1", "ARM 2"), ARMCD = c("ARM A", "ARM B"))
-#'
-#' ui <- fluidPage(
-#'   sidebarLayout(
-#'     sidebarPanel(
-#'       optionalSelectInput(
-#'         "arm",
-#'         "Treatment Variable",
-#'         choices = arm_var$choices,
-#'         selected = arm_var$selected
-#'       ),
-#'       uiOutput("arms_buckets")
-#'     ),
-#'     mainPanel(
-#'       textOutput("result")
-#'     )
-#'   )
-#' )
-#'
-#' server <- function(input, output, session) {
-#'   iv_arm_ref <- arm_ref_comp_observer(
-#'     session,
-#'     input,
-#'     output,
-#'     id_arm_var = "arm",
-#'     data = adsl,
-#'     arm_ref_comp = arm_ref_comp,
-#'     module = "example"
-#'   )
-#'
-#'   output$result <- renderText({
-#'     iv <- InputValidator$new()
-#'     iv$add_validator(iv_arm_ref)
-#'     iv$enable()
-#'     validate_inputs(iv)
-#'     "Valid selection has been made!"
-#'   })
-#' }
-#' if (interactive()) {
-#'   shinyApp(ui, server)
-#' }
 arm_ref_comp_observer <- function(session,
                                   input,
                                   output,
