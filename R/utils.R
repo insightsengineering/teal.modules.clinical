b98d72a4c3b1dd70f7002622ef81e0c04ed2a5fc
Aleksander Chlebowski <aleksander.chlebowski@contractors.roche.com>
2024-02-22 19:52:14 +0100

    update deprecation message in get_var_labels

--- R/utils.R
+++ R/utils.R
@@ -65,7 +65,7 @@ get_var_labels <- function(datasets, dataname, vars) {
   lifecycle::deprecate_warn(
     when = "0.8.14",
     what = "get_var_labels()",
-    with = "formatters::var_labels()",
+    with = "teal.data::col_labels()",
     details = "teal.modules.clinical won't export any utility functions except those which
       are necessary to prepare shiny app."
   )
