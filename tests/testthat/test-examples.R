6cfba95fff662ec432d25f94d0b71d3c52a27204
Pawel Rucki <12943682+pawelru@users.noreply.github.com>
2024-03-11 11:34:10 +0100

    symlink man dir; cleanups

--- tests/testthat/test-examples.R
+++ tests/testthat/test-examples.R
@@ -1,9 +1,12 @@
-files <- devtools:::rd_files()
+# this test requires a `man` directory in the `tests/testthat` directory
+# (presumably symlinked to the package root `man` directory to avoid duplication)
+# this also requires `devtools::document()` to be run before running the tests
 
-# this is temporary
-# every app tested with shinytest2 throws a warning because of https://github.com/insightsengineering/teal.code/issues/194
-# silent certain warnings
-suppress_warnings <- function(expr, pattern, ...) {
+rd_files <- function() {
+  list.files(testthat::test_path("man"), pattern = "\\.[Rr]d$", full.names = TRUE)
+}
+
+suppress_warnings <- function(expr, pattern = "*", ...) {
   withCallingHandlers(expr, warning = function(w) {
     if (grepl(pattern, conditionMessage(w))) {
       invokeRestart("muffleWarning")
@@ -11,21 +14,22 @@ suppress_warnings <- function(expr, pattern, ...) {
   })
 }
 
-for (i in files) {
-  with_mocked_bindings(
-    test_that(
-      paste0("example-", basename(i)),{
-        skip_on_cran()
-        expect_no_error(
-          suppress_warnings( # temporary (see above)
+for (i in rd_files()) {
+  testthat::with_mocked_bindings(
+    testthat::test_that(
+      paste0("example-", basename(i)),
+      {
+        testthat::skip_on_cran()
+        testthat::expect_no_error(
+          # surpress warnings coming from saving qenv https://github.com/insightsengineering/teal.code/issues/194
+          suppress_warnings(
             pkgload::run_example(i, run_donttest = TRUE, run_dontrun = FALSE, quiet = TRUE),
             "may not be available when loading"
           )
         )
       }
     ),
-    runApp = \(appDir, ...) {
-      cat("hello from mocked runApp\n")
+    runApp = function(appDir, ...) { # nolint object_name_linter.
       app <- shinytest2::AppDriver$new(appDir)
       app$stop()
     },
