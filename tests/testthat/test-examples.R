4279a72b0017d9939f1e4d8c9cf2b5bea03fa627
Pawel Rucki <12943682+pawelru@users.noreply.github.com>
2024-03-21 12:25:00 +0100

    quiet=FALSE; better error capture

--- tests/testthat/test-examples.R
+++ tests/testthat/test-examples.R
@@ -21,17 +21,29 @@ for (i in rd_files()) {
       {
         testthat::skip_on_cran()
         testthat::expect_no_error(
-          # surpress warnings coming from saving qenv https://github.com/insightsengineering/teal.code/issues/194
-          suppress_warnings(
-            pkgload::run_example(i, run_donttest = TRUE, run_dontrun = FALSE, quiet = TRUE),
-            "may not be available when loading"
+          capture.output(
+            # surpress warnings coming from saving qenv https://github.com/insightsengineering/teal.code/issues/194
+            suppress_warnings(
+              # quiet argument must be FALSE - otherwise the shiny apps are not invoked
+              pkgload::run_example(i, run_donttest = TRUE, run_dontrun = FALSE, quiet = FALSE),
+              "may not be available when loading"
+            )
           )
         )
       }
     ),
     runApp = function(appDir, ...) { # nolint object_name_linter.
-      app <- shinytest2::AppDriver$new(appDir)
-      app$stop()
+      app_driver <- shinytest2::AppDriver$new(appDir)
+      on.exit(app_driver$stop)
+      # shinytest2 will capture app crash but actually teal continues on error inside the module
+      # we need to use a different way to check if there are errors
+      if (!is.null(app_driver$get_html(".shiny-output-error:not(.shiny-output-error-validation)"))) {
+        stop("module error is observed")
+      }
+      # validation errors from shinyvalidate - added by default to assure the examples are "clean"
+      if (!is.null(app_driver$get_html(".shiny-input-container.has-error"))) {
+        stop("shinyvalidate error is observed")
+      }
     },
     .package = "shiny"
   )
