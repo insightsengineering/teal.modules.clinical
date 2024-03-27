2b1e1a72b55005331dde7860f33654807c53fd0c
Aleksander Chlebowski <aleksander.chlebowski@contractors.roche.com>
2024-03-27 12:08:30 +0100

    bookmarkable flag: tm_g_pp_adverse_events

--- R/tm_g_pp_adverse_events.R
+++ R/tm_g_pp_adverse_events.R
@@ -314,7 +314,8 @@ tm_g_pp_adverse_events <- function(label,
     ),
     datanames = c(dataname, parentname)
   )
-  attr(ans, "teal_bookmarkable") <- NULL
+  # presence of data extracts in UI may cause bookmarking problems as in other modules
+  attr(ans, "teal_bookmarkable") <- TRUE
   ans
 }
 
@@ -348,12 +349,7 @@ ui_g_adverse_events <- function(id, ...) {
         "aeterm", "tox_grade", "causality", "outcome",
         "action", "time", "decod"
       )]),
-      teal.widgets::optionalSelectInput(
-        ns("patient_id"),
-        "Select Patient:",
-        multiple = FALSE,
-        options = shinyWidgets::pickerOptions(`liveSearch` = TRUE)
-      ),
+      uiOutput(ns("container_patient_id")),
       teal.transform::data_extract_ui(
         id = ns("aeterm"),
         label = "Select AETERM variable:",
@@ -449,33 +445,31 @@ srv_g_adverse_events <- function(id,
 
     patient_id <- reactive(input$patient_id)
 
-    # Init
     patient_data_base <- reactive(unique(data()[[parentname]][[patient_col]]))
-    teal.widgets::updateOptionalSelectInput(
-      session,
-      "patient_id",
-      choices = patient_data_base(),
-      selected = restoreInput(ns("patient_id"), patient_data_base()[1])
-    )
 
-    observeEvent(patient_data_base(),
-      handlerExpr = {
-        teal.widgets::updateOptionalSelectInput(
-          session,
-          "patient_id",
-          choices = patient_data_base(),
-          selected = restoreInput(
-            ns("patient_id"),
-            if (length(patient_data_base()) == 1) {
-              patient_data_base()
-            } else {
-              intersect(patient_id(), patient_data_base())
-            }
-          )
-        )
-      },
-      ignoreInit = TRUE
-    )
+    output$container_patient_id <- renderUI({
+      req(patient_data_base())
+
+      selected <-
+        if (!isTruthy(patient_id())) {
+          patient_data_base()[1L]
+        } else {
+          if (length(patient_data_base()) == 1) {
+            patient_data_base()
+          } else {
+            intersect(patient_id(), patient_data_base())
+          }
+        }
+
+      teal.widgets::optionalSelectInput(
+        ns("patient_id"),
+        "Select Patient:",
+        choices = patient_data_base(),
+        selected = selected,
+        multiple = FALSE,
+        options = shinyWidgets::pickerOptions(`liveSearch` = TRUE)
+      )
+    })
 
     # Adverse events tab ----
     selector_list <- teal.transform::data_extract_multiple_srv(
