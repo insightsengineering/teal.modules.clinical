b8894bd48d46e42daaf073f2c4161d94debed4a6
Pawel Rucki <12943682+pawelru@users.noreply.github.com>
2024-04-02 17:50:42 +0200

    more DRY call; implement workaround

--- tests/testthat/test-examples.R
+++ tests/testthat/test-examples.R
@@ -16,8 +16,15 @@ suppress_warnings <- function(expr, pattern = "*", ...) {
 
 with_mocked_app_bindings <- function(code) {
   shiny__shinyApp <- shiny::shinyApp # nolint object_name_linter.
-  mocked_shinyApp <- function(...) { # nolint object_name_linter.
-    print(shiny__shinyApp(...))
+
+  # workaround of https://github.com/rstudio/shinytest2/issues/381
+  # change to `print(shiny__shinyApp(...))` once fixed
+  mocked_shinyApp <- function(ui, server, ...) { # nolint object_name_linter.
+    functionBody(server) <- bquote({
+      library(.(testthat::testing_package()), character.only = TRUE)
+      .(functionBody(server))
+    })
+    print(do.call(shiny__shinyApp, append(x = list(ui = ui, server = server), list(...))))
   }
 
   mocked_runApp <- function(x, ...) { # nolint object_name_linter.
@@ -41,8 +48,8 @@ with_mocked_app_bindings <- function(code) {
     if (any(grepl("Warning in.*", app_logs))) {
       warning(
         sprintf(
-          "Detected following warning(s) (a message might be incomplete):\n%s",
-          paste0("* ", grep("Warning in.*", app_logs, value = TRUE), collapse = "\n")
+          "Detected a warning in the application logs:\n%s",
+          paste0(app_logs, collapse = "\n")
         )
       )
     }
@@ -62,7 +69,10 @@ with_mocked_app_bindings <- function(code) {
     }
   }
 
-  # mock both local and package bindings to cover both `shinyApp(...)` and `shiny::shinyApp(...)` calls
+  # support both `shinyApp(...)` as well as prefixed `shiny::shinyApp(...)` calls
+  # mock `shinyApp` to `shiny::shinyApp` and `shiny::shinyApp` to custom function
+  # same for `runApp(...)` and `shiny::runApp`
+  # additionally mock `interactive()`
   testthat::with_mocked_bindings(
     testthat::with_mocked_bindings(
       code,
@@ -86,24 +96,20 @@ strict_exceptions <- c(
 )
 
 for (i in rd_files()) {
-  with_mocked_app_bindings(
-    testthat::test_that(
-      paste0("example-", basename(i)),
-      {
-        testthat::skip_on_cran()
-        if (basename(i) %in% strict_exceptions) {
-          withr::with_options(
-            opts_partial_match_old,
-            testthat::expect_no_error(
-              pkgload::run_example(i, run_donttest = TRUE, run_dontrun = FALSE, quiet = TRUE)
-            )
-          )
-        } else {
-          testthat::expect_no_error(
-            pkgload::run_example(i, run_donttest = TRUE, run_dontrun = FALSE, quiet = TRUE)
-          )
-        }
+  testthat::test_that(
+    paste0("example-", basename(i)),
+    {
+      testthat::skip_on_cran()
+      if (basename(i) %in% strict_exceptions) {
+        op <- options()
+        withr::local_options(opts_partial_match_old)
+        withr::defer(options(op))
       }
-    )
+      with_mocked_app_bindings(
+        testthat::expect_no_error(
+          pkgload::run_example(i, run_donttest = TRUE, run_dontrun = FALSE, quiet = TRUE)
+        )
+      )
+    }
   )
 }
