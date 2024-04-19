800ff8da79a29dfdbe63b672755986a71ad035a6
André Veríssimo <211358+averissimo@users.noreply.github.com>
2024-04-19 14:35:25 +0200

    fix: change function

--- tests/testthat/helper-TealAppDriver.R
+++ tests/testthat/helper-TealAppDriver.R
@@ -6,13 +6,13 @@
 init_teal_app_driver <- function(...) {
   testthat::with_mocked_bindings(
     {
-      TealAppDriver <- getFromNamespace("TealAppDriver", "teal") # nolint: object_name.
+      # TealAppDriver <- getFromNamespace("TealAppDriver", "teal") # nolint: object_name.
       TealAppDriver$new(...)
     },
     shinyApp = function(ui, server, ...) {
       functionBody(server) <- bquote({
         # Hint to shinytest2 that this package should be available (via {globals})
-        .hint_to_load_package <- add_facet_labels
+        .hint_to_load_package <- tm_g_ci # Hint to shinytest2 when looking for packages in globals
         .(functionBody(server))
       })
 
