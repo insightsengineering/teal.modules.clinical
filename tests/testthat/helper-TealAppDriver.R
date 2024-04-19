cc7388d3837bc290ff45699de1206e5389375459
André Veríssimo <211358+averissimo@users.noreply.github.com>
2024-04-19 14:31:45 +0200

    feat: adds helper to use TealAppDriver and skip_if_too_deep

---
+++ tests/testthat/helper-TealAppDriver.R
@@ -0,0 +1,25 @@
+# Initialization function to create a new TealAppDriver object
+#
+# By manipulating the server function as below, we can hint {shinytest2} to load
+# this package and its "Depends".
+# Related to https://github.com/rstudio/shinytest2/issues/381
+init_teal_app_driver <- function(...) {
+  testthat::with_mocked_bindings(
+    {
+      TealAppDriver <- getFromNamespace("TealAppDriver", "teal") # nolint: object_name.
+      TealAppDriver$new(...)
+    },
+    shinyApp = function(ui, server, ...) {
+      functionBody(server) <- bquote({
+        # Hint to shinytest2 that this package should be available (via {globals})
+        .hint_to_load_package <- add_facet_labels
+        .(functionBody(server))
+      })
+
+      shiny::shinyApp(ui, server, ...)
+    },
+    # The relevant shinyApp call in `TealAppDriver` is being called without prefix,
+    # hence why the package bindings that is changed is in {teal} and not {shiny}
+    .package = "teal"
+  )
+}
