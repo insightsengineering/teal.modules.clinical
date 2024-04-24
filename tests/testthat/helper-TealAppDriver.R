7b40b9dc23e288db268d0f2a2a11e06c8a603dff
m7pr <marcin.kosinski.mk1@roche.com>
2024-04-24 11:41:53 +0200

    use get_active_module_tws_output

--- tests/testthat/helper-TealAppDriver.R
+++ tests/testthat/helper-TealAppDriver.R
@@ -23,16 +23,3 @@ init_teal_app_driver <- function(...) {
     .package = "teal"
   )
 }
-
-# Returns data.frame with table output
-active_module_tws_output <- function(app_driver, element = "table-table-with-settings", which = 1) {
-  app_driver$active_module_element(element) %>%
-    app_driver$get_html_rvest() %>%
-    rvest::html_table(fill = TRUE) %>% {
-      if (identical(., list())) {
-        .
-      } else {
-        .[[which]]
-      }
-    }
-}
