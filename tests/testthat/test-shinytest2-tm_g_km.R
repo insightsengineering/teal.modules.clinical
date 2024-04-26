b7b84141a087b2acc9985014383a849be1f05ca7
André Veríssimo <211358+averissimo@users.noreply.github.com>
2024-04-26 12:10:33 +0200

    fix: linter hanging indentation

--- tests/testthat/test-shinytest2-tm_g_km.R
+++ tests/testthat/test-shinytest2-tm_g_km.R
@@ -49,13 +49,19 @@ app_driver_tm_g_km <- function() {
         NULL
       ),
       time_unit_var = teal.transform::choices_selected(
-        teal.transform::variable_choices(data[["ADTTE"]], "VALUE_UNIT"), "VALUE_UNIT", fixed = TRUE
+        teal.transform::variable_choices(data[["ADTTE"]], "VALUE_UNIT"),
+        "VALUE_UNIT",
+        fixed = TRUE
       ),
       aval_var = teal.transform::choices_selected(
-        teal.transform::variable_choices(data[["ADTTE"]], "ANALYSIS_VAL"), "ANALYSIS_VAL", fixed = TRUE
+        teal.transform::variable_choices(data[["ADTTE"]], "ANALYSIS_VAL"),
+        "ANALYSIS_VAL",
+        fixed = TRUE
       ),
       cnsr_var = teal.transform::choices_selected(
-        teal.transform::variable_choices(data[["ADTTE"]], "CENSORING"), "CENSORING", fixed = TRUE
+        teal.transform::variable_choices(data[["ADTTE"]], "CENSORING"),
+        "CENSORING",
+        fixed = TRUE
       ),
       conf_level = teal.transform::choices_selected(c(0.95, 0.9, 0.8, -1), 0.95, keep_order = TRUE)
     )
@@ -76,48 +82,50 @@ testthat::test_that("e2e - tm_g_km: Module initializes in teal without errors an
 })
 
 testthat::test_that(
-  "e2e - tm_g_km: Starts with specified paramcd, aval_var, cnsr_var, facet_var, arm_var, compare_arms, strata_var.", {
-  skip_if_too_deep(5)
-  app_driver <- app_driver_tm_g_km()
+  "e2e - tm_g_km: Starts with specified paramcd, aval_var, cnsr_var, facet_var, arm_var, compare_arms, strata_var.",
+  {
+    skip_if_too_deep(5)
+    app_driver <- app_driver_tm_g_km()
 
-  testthat::expect_equal(
-    app_driver$get_text("#teal-main_ui-root-active_tab > li.active > a"),
-    "Kaplan-Meier Plot"
-  )
+    testthat::expect_equal(
+      app_driver$get_text("#teal-main_ui-root-active_tab > li.active > a"),
+      "Kaplan-Meier Plot"
+    )
 
-  testthat::expect_equal(
-    app_driver$get_active_module_input("paramcd-dataset_ADTTE_singleextract-filter1-vals"),
-    "OS"
-  )
+    testthat::expect_equal(
+      app_driver$get_active_module_input("paramcd-dataset_ADTTE_singleextract-filter1-vals"),
+      "OS"
+    )
 
-  testthat::expect_equal(
-    app_driver$get_active_module_input("aval_var-dataset_ADTTE_singleextract-select"),
-    "ANALYSIS_VAL"
-  )
+    testthat::expect_equal(
+      app_driver$get_active_module_input("aval_var-dataset_ADTTE_singleextract-select"),
+      "ANALYSIS_VAL"
+    )
 
-  testthat::expect_equal(
-    app_driver$get_active_module_input("cnsr_var-dataset_ADTTE_singleextract-select"),
-    "CENSORING"
-  )
+    testthat::expect_equal(
+      app_driver$get_active_module_input("cnsr_var-dataset_ADTTE_singleextract-select"),
+      "CENSORING"
+    )
 
-  testthat::expect_equal(
-    app_driver$get_active_module_input("facet_var-dataset_ADSL_singleextract-select"),
-    NULL
-  )
+    testthat::expect_equal(
+      app_driver$get_active_module_input("facet_var-dataset_ADSL_singleextract-select"),
+      NULL
+    )
 
-  testthat::expect_equal(
-    app_driver$get_active_module_input("arm_var-dataset_ADSL_singleextract-select"),
-    "ARM"
-  )
+    testthat::expect_equal(
+      app_driver$get_active_module_input("arm_var-dataset_ADSL_singleextract-select"),
+      "ARM"
+    )
 
-  testthat::expect_true(app_driver$get_active_module_input("compare_arms"))
+    testthat::expect_true(app_driver$get_active_module_input("compare_arms"))
 
-  testthat::expect_equal(
-    app_driver$get_active_module_input("strata_var-dataset_ADSL_singleextract-select"),
-    "SEX"
-  )
-  app_driver$stop()
-})
+    testthat::expect_equal(
+      app_driver$get_active_module_input("strata_var-dataset_ADSL_singleextract-select"),
+      "SEX"
+    )
+    app_driver$stop()
+  }
+)
 
 testthat::test_that("e2e - tm_g_km: Changing {paramcd} changes the plot without errors.", {
   skip_if_too_deep(5)
