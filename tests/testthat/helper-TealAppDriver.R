2ad7f7c0fcb674e3525c1c31cc88e8f2b25c89ad
unknown <kirar.kartikeya1@gmail.com>
2024-04-26 13:07:24 +0530

    fix ci error

--- tests/testthat/helper-TealAppDriver.R
+++ tests/testthat/helper-TealAppDriver.R
@@ -31,7 +31,7 @@ test_no_validation_error <- function(app_driver, input_id, value) {
 
 ns_dataset <- function(prefix, suffix, dataset, extract = "singleextract") {
   sprintf("%s-dataset_%s_%s-%s", prefix, dataset, extract, suffix)
-  
+}
 # returns base 64 encoded image
 active_module_pws_output <- function(app_driver) {
   app_driver$get_attr(
