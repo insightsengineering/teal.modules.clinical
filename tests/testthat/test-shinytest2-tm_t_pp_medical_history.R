b5ef0b640e9e33db54a7c1afd470d8817403e980
vedhav <vedhaviyash4@gmail.com>
2024-05-01 02:12:17 +0530

    feat: setup shinytest2 for tm_t_pp_medical_history

---
+++ tests/testthat/test-shinytest2-tm_t_pp_medical_history.R
@@ -0,0 +1,75 @@
+app_driver_tm_t_pp_medical_history <- function() { # nolint: object_length
+  data <- teal_data()
+  data <- within(data, {
+    ADSL <- tmc_ex_adsl
+    ADMH <- tmc_ex_admh
+  })
+  datanames <- c("ADSL", "ADMH")
+  teal.data::datanames(data) <- datanames
+  teal.data::join_keys(data) <- teal.data::default_cdisc_join_keys[datanames]
+  init_teal_app_driver(
+    data = data,
+    modules = tm_t_pp_medical_history(
+      label = "Medical History",
+      dataname = "ADMH",
+      parentname = "ADSL",
+      patient_col = "USUBJID",
+      mhterm = teal.transform::choices_selected(
+        choices = teal.transform::variable_choices(data[["ADMH"]], c("MHTERM")),
+        selected = "MHTERM"
+      ),
+      mhbodsys = teal.transform::choices_selected(
+        choices = teal.transform::variable_choices(data[["ADMH"]], "MHBODSYS"),
+        selected = "MHBODSYS"
+      ),
+      mhdistat = teal.transform::choices_selected(
+        choices = teal.transform::variable_choices(data[["ADMH"]], "MHDISTAT"),
+        selected = "MHDISTAT"
+      )
+    )
+  )
+}
+
+testthat::test_that(
+  "e2e - tm_t_pp_medical_history: Module initializes in teal without errors and produces table output.",
+  {
+    skip_if_too_deep(5)
+    app_driver <- app_driver_tm_t_pp_medical_history()
+    app_driver$expect_no_shiny_error()
+    app_driver$expect_no_validation_error()
+    testthat::expect_true(
+      app_driver$is_visible(app_driver$active_module_element("table-table-with-settings"))
+    )
+    app_driver$stop()
+  }
+)
+
+testthat::test_that(
+  "e2e - tm_t_pp_medical_history: Starts with specified label, patient_id, mhterm, mhbodsys, mhdistat.",
+  {
+    skip_if_too_deep(5)
+    app_driver <- app_driver_tm_t_pp_medical_history()
+
+    testthat::expect_equal(
+      app_driver$get_text("#teal-main_ui-root-active_tab > li.active > a"),
+      "Medical History"
+    )
+    testthat::expect_equal(
+      app_driver$get_active_module_input("patient_id"),
+      "AB12345-CHN-1-id-1"
+    )
+    testthat::expect_equal(
+      app_driver$get_active_module_input("mhterm-dataset_ADMH_singleextract-select"),
+      "MHTERM"
+    )
+    testthat::expect_equal(
+      app_driver$get_active_module_input("mhbodsys-dataset_ADMH_singleextract-select"),
+      "MHBODSYS"
+    )
+    testthat::expect_equal(
+      app_driver$get_active_module_input("mhdistat-dataset_ADMH_singleextract-select"),
+      "MHDISTAT"
+    )
+    app_driver$stop()
+  }
+)
