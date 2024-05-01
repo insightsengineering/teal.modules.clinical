311c0fe063039d60cf3001e3c858a49ca17c0607
unknown <kirar.kartikeya1@gmail.com>
2024-05-02 00:24:31 +0530

    adding test for inputs validation.

--- tests/testthat/test-shinytest2-tm_t_events_by_grade.R
+++ tests/testthat/test-shinytest2-tm_t_events_by_grade.R
@@ -90,3 +90,89 @@ testthat::test_that(
     app_driver$stop()
   }
 )
+
+testthat::test_that("e2e - tm_t_events_by_grade: Selecting arm_var-variable changes the table and does not throw validation errors.", {
+  skip_if_too_deep(5)
+  app_driver <- app_driver_tm_t_events_by_grade()
+  table_before <- app_driver$get_active_module_tws_output("table")
+  app_driver$set_active_module_input("arm_var-dataset_ADSL_singleextract-select", "ARMCD")
+  testthat::expect_false(identical(table_before, app_driver$get_active_module_tws_output("table")))
+  app_driver$expect_no_validation_error()
+  app_driver$stop()
+})
+
+testthat::test_that("e2e - tm_t_events_by_grade: Deselection of arm_var-variable throws validation error.", {
+  skip_if_too_deep(5)
+  app_driver <- app_driver_tm_t_events_by_grade()
+  app_driver$set_active_module_input("arm_var-dataset_ADSL_singleextract-select", NULL)
+  testthat::expect_identical(app_driver$get_active_module_tws_output("table"), data.frame())
+  app_driver$expect_validation_error()
+  testthat::expect_equal(
+    app_driver$active_module_element_text("arm_var-dataset_ADSL_singleextract-select_input .shiny-validation-message"),
+    "A treatment variable is required"
+  )
+  app_driver$stop()
+})
+
+testthat::test_that("e2e - tm_t_events_by_grade: Selecting hlt-variable changes the table and does not throw validation errors.", {
+  skip_if_too_deep(5)
+  app_driver <- app_driver_tm_t_events_by_grade()
+  table_before <- app_driver$get_active_module_tws_output("table")
+  app_driver$set_active_module_input("hlt-dataset_ADAE_singleextract-select", "AESOC")
+  testthat::expect_false(identical(table_before, app_driver$get_active_module_tws_output("table")))
+  app_driver$expect_no_validation_error()
+  app_driver$stop()
+})
+
+testthat::test_that("e2e - tm_t_events_by_grade: Deselection of hlt-variable changes the table and does not throw validation errors.", {
+  skip_if_too_deep(5)
+  app_driver <- app_driver_tm_t_events_by_grade()
+  table_before <- app_driver$get_active_module_tws_output("table")
+  app_driver$set_active_module_input("hlt-dataset_ADAE_singleextract-select", NULL)
+  testthat::expect_false(identical(table_before, app_driver$get_active_module_tws_output("table")))
+  app_driver$expect_no_validation_error()
+  app_driver$stop()
+})
+
+testthat::test_that("e2e - tm_t_events_by_grade: Selecting llt-variable changes the table and does not throw validation errors.", {
+  skip_if_too_deep(5)
+  app_driver <- app_driver_tm_t_events_by_grade()
+  table_before <- app_driver$get_active_module_tws_output("table")
+  app_driver$set_active_module_input("llt-dataset_ADAE_singleextract-select", "AETERM")
+  testthat::expect_false(identical(table_before, app_driver$get_active_module_tws_output("table")))
+  app_driver$expect_no_validation_error()
+  app_driver$stop()
+})
+
+testthat::test_that("e2e - tm_t_events_by_grade: Deselection of llt-variable changes the table and does not throw validation errors.", {
+  skip_if_too_deep(5)
+  app_driver <- app_driver_tm_t_events_by_grade()
+  table_before <- app_driver$get_active_module_tws_output("table")
+  app_driver$set_active_module_input("llt-dataset_ADAE_singleextract-select", NULL)
+  testthat::expect_false(identical(table_before, app_driver$get_active_module_tws_output("table")))
+  app_driver$expect_no_validation_error()
+  app_driver$stop()
+})
+
+testthat::test_that("e2e - tm_t_events_by_grade: Selecting grade-variable changes the table and does not throw validation errors.", {
+  skip_if_too_deep(5)
+  app_driver <- app_driver_tm_t_events_by_grade()
+  table_before <- app_driver$get_active_module_tws_output("table")
+  app_driver$set_active_module_input("grade-dataset_ADAE_singleextract-select", "AESEV")
+  testthat::expect_false(identical(table_before, app_driver$get_active_module_tws_output("table")))
+  app_driver$expect_no_validation_error()
+  app_driver$stop()
+})
+
+testthat::test_that("e2e - tm_t_events_by_grade: Deselection of grade-variable throws validation error.", {
+  skip_if_too_deep(5)
+  app_driver <- app_driver_tm_t_events_by_grade()
+  app_driver$set_active_module_input("grade-dataset_ADAE_singleextract-select", NULL)
+  testthat::expect_identical(app_driver$get_active_module_tws_output("table"), data.frame())
+  app_driver$expect_validation_error()
+  testthat::expect_equal(
+    app_driver$active_module_element_text("grade-dataset_ADAE_singleextract-select_input .shiny-validation-message"),
+    "An event grade is required"
+  )
+  app_driver$stop()
+})
