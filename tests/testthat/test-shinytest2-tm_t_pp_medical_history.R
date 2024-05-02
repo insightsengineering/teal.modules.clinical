a9dd1fd83dbb8be17a4de28370badf76b38b7c5b
unknown <kirar.kartikeya1@gmail.com>
2024-05-02 12:01:38 +0530

    adding validation test for input.

--- tests/testthat/test-shinytest2-tm_t_pp_medical_history.R
+++ tests/testthat/test-shinytest2-tm_t_pp_medical_history.R
@@ -73,3 +73,65 @@ testthat::test_that(
     app_driver$stop()
   }
 )
+
+testthat::test_that("e2e - tm_t_pp_medical_history: Selecting patient_id changes the table and does not throw validation errors.", {
+  skip_if_too_deep(5)
+  app_driver <- app_driver_tm_t_pp_medical_history()
+  table_before <- app_driver$get_active_module_tws_output("table")
+  app_driver$set_active_module_input("patient_id", "AB12345-USA-1-id-261")
+  testthat::expect_false(identical(table_before, app_driver$get_active_module_tws_output("table")))
+  app_driver$expect_no_validation_error()
+  app_driver$stop()
+})
+
+testthat::test_that("e2e - tm_t_pp_medical_history: Deselection of patient_id throws validation error.", {
+  skip_if_too_deep(5)
+  app_driver <- app_driver_tm_t_pp_medical_history()
+  app_driver$set_active_module_input("patient_id", NULL)
+  testthat::expect_identical(app_driver$get_active_module_tws_output("table"), data.frame())
+  app_driver$expect_validation_error()
+  testthat::expect_equal(
+    app_driver$active_module_element_text("patient_id_input .shiny-validation-message"),
+    "Please select a patient"
+  )
+  app_driver$stop()
+})
+
+testthat::test_that("e2e - tm_t_pp_medical_history: Deselection of mhterm-variable throws validation error.", {
+
+  app_driver <- app_driver_tm_t_pp_medical_history()
+  app_driver$set_active_module_input("mhterm-dataset_ADMH_singleextract-select", NULL)
+  testthat::expect_identical(app_driver$get_active_module_tws_output("table"), data.frame())
+  app_driver$expect_validation_error()
+  testthat::expect_equal(
+    app_driver$active_module_element_text("mhterm-dataset_ADMH_singleextract-select_input .shiny-validation-message"),
+    "Please select MHTERM variable."
+  )
+  app_driver$stop()
+})
+
+testthat::test_that("e2e - tm_t_pp_medical_history: Deselection of mhbodsys-variable throws validation error.", {
+
+  app_driver <- app_driver_tm_t_pp_medical_history()
+  app_driver$set_active_module_input("mhbodsys-dataset_ADMH_singleextract-select", NULL)
+  testthat::expect_identical(app_driver$get_active_module_tws_output("table"), data.frame())
+  app_driver$expect_validation_error()
+  testthat::expect_equal(
+    app_driver$active_module_element_text("mhbodsys-dataset_ADMH_singleextract-select_input .shiny-validation-message"),
+    "Please select MHBODSYS variable."
+  )
+  app_driver$stop()
+})
+
+testthat::test_that("e2e - tm_t_pp_medical_history: Deselection of mhdistat-variable throws validation error.", {
+
+  app_driver <- app_driver_tm_t_pp_medical_history()
+  app_driver$set_active_module_input("mhdistat-dataset_ADMH_singleextract-select", NULL)
+  testthat::expect_identical(app_driver$get_active_module_tws_output("table"), data.frame())
+  app_driver$expect_validation_error()
+  testthat::expect_equal(
+    app_driver$active_module_element_text("mhdistat-dataset_ADMH_singleextract-select_input .shiny-validation-message"),
+    "Please select MHDISTAT variable."
+  )
+  app_driver$stop()
+})
