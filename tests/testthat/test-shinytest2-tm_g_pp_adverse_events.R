c96ca0c3e0693c0459558b9a616684dbf4a4267e
vedhav <vedhaviyash4@gmail.com>
2024-05-03 18:45:10 +0530

    chore: fix lint errors

--- tests/testthat/test-shinytest2-tm_g_pp_adverse_events.R
+++ tests/testthat/test-shinytest2-tm_g_pp_adverse_events.R
@@ -1,4 +1,4 @@
-app_driver_tm_g_pp_adverse_events <- function() {
+app_driver_tm_g_pp_adverse_events <- function() { # nolint: object_length
   data <- teal.data::teal_data() |> within({
     library(nestcolor)
     library(dplyr)
@@ -53,7 +53,7 @@ app_driver_tm_g_pp_adverse_events <- function() {
 testthat::test_that(
   "e2e tm_g_pp_adverse_events - Module initializes in teal without any errors and produces a plot.",
   {
-    # skip_if_too_deep(5)
+    skip_if_too_deep(5)
     app_driver <- app_driver_tm_g_pp_adverse_events()
     app_driver$expect_no_shiny_error()
     app_driver$expect_no_validation_error()
@@ -63,9 +63,10 @@ testthat::test_that(
 )
 
 testthat::test_that(
-  "e2e tm_g_pp_adverse_events - Module initializes with specific label, patient_id, aeterm, tox_grade, causality, outcome, action, time, decod.",
+  "e2e tm_g_pp_adverse_events - Module initializes with specific label, patient_id, aeterm,
+  tox_grade, causality, outcome, action, time, decod.",
   {
-    # skip_if_too_deep(5)
+    skip_if_too_deep(5)
     app_driver <- app_driver_tm_g_pp_adverse_events()
     testthat::expect_equal(
       app_driver$get_text("#teal-main_ui-root-active_tab > li.active > a"),
@@ -118,7 +119,7 @@ testthat::test_that(
 testthat::test_that(
   "e2e tm_g_pp_adverse_events - Selecting patient_id doesn't throw errors and changes a plot",
   {
-    # skip_if_too_deep(5)
+    skip_if_too_deep(5)
     app_driver <- app_driver_tm_g_pp_adverse_events()
     plot_before <- app_driver$get_active_module_pws_output("chart")
     app_driver$set_active_module_input("patient_id", "AB12345-CHN-15-id-262")
@@ -136,7 +137,7 @@ testthat::test_that(
 testthat::test_that(
   "e2e tm_g_pp_adverse_events - Deselecting patient_id throw validation error",
   {
-    # skip_if_too_deep(5)
+    skip_if_too_deep(5)
     app_driver <- app_driver_tm_g_pp_adverse_events()
     input_id <- "patient_id"
     app_driver$set_active_module_input(input_id, "")
@@ -152,7 +153,7 @@ testthat::test_that(
 testthat::test_that(
   "e2e tm_g_pp_adverse_events - Selecting aeterm column doesn't throw errors and changes a plot",
   {
-    # skip_if_too_deep(5)
+    skip_if_too_deep(5)
     # not possible for given app due to single choice
   }
 )
@@ -160,7 +161,7 @@ testthat::test_that(
 testthat::test_that(
   "e2e tm_g_pp_adverse_events - Deselecting aeterm column throw validation error",
   {
-    # skip_if_too_deep(5)
+    skip_if_too_deep(5)
     app_driver <- app_driver_tm_g_pp_adverse_events()
     input_id <- "aeterm-dataset_ADAE_singleextract-select"
     app_driver$set_active_module_input(input_id, "")
@@ -176,7 +177,7 @@ testthat::test_that(
 testthat::test_that(
   "e2e tm_g_pp_adverse_events - Selecting tox_grade column doesn't throw errors and changes a plot",
   {
-    # skip_if_too_deep(5)
+    skip_if_too_deep(5)
     # not possible for given app due to single choice
   }
 )
@@ -184,7 +185,7 @@ testthat::test_that(
 testthat::test_that(
   "e2e tm_g_pp_adverse_events - Deselecting tox_grade column throw validation error",
   {
-    # skip_if_too_deep(5)
+    skip_if_too_deep(5)
     app_driver <- app_driver_tm_g_pp_adverse_events()
     input_id <- "tox_grade-dataset_ADAE_singleextract-select"
     app_driver$set_active_module_input(input_id, "")
@@ -200,7 +201,7 @@ testthat::test_that(
 testthat::test_that(
   "e2e tm_g_pp_adverse_events - Selecting causality column doesn't throw errors and changes a plot",
   {
-    # skip_if_too_deep(5)
+    skip_if_too_deep(5)
     # not possible for given app due to single choice
   }
 )
@@ -208,7 +209,7 @@ testthat::test_that(
 testthat::test_that(
   "e2e tm_g_pp_adverse_events - Deselecting causality column throw validation error",
   {
-    # skip_if_too_deep(5)
+    skip_if_too_deep(5)
     app_driver <- app_driver_tm_g_pp_adverse_events()
     input_id <- "causality-dataset_ADAE_singleextract-select"
     app_driver$set_active_module_input(input_id, "")
@@ -224,7 +225,7 @@ testthat::test_that(
 testthat::test_that(
   "e2e tm_g_pp_adverse_events - Selecting outcome column doesn't throw errors and changes a plot",
   {
-    # skip_if_too_deep(5)
+    skip_if_too_deep(5)
     # not possible for given app due to single choice
   }
 )
@@ -232,7 +233,7 @@ testthat::test_that(
 testthat::test_that(
   "e2e tm_g_pp_adverse_events - Deselecting outcome column throw validation error",
   {
-    # skip_if_too_deep(5)
+    skip_if_too_deep(5)
     app_driver <- app_driver_tm_g_pp_adverse_events()
     input_id <- "outcome-dataset_ADAE_singleextract-select"
     app_driver$set_active_module_input(input_id, "")
@@ -248,7 +249,7 @@ testthat::test_that(
 testthat::test_that(
   "e2e tm_g_pp_adverse_events - Selecting action column doesn't throw errors and changes a plot",
   {
-    # skip_if_too_deep(5)
+    skip_if_too_deep(5)
     # not possible for given app due to single choice
   }
 )
@@ -256,7 +257,7 @@ testthat::test_that(
 testthat::test_that(
   "e2e tm_g_pp_adverse_events - Deselecting action column throw validation error",
   {
-    # skip_if_too_deep(5)
+    skip_if_too_deep(5)
     app_driver <- app_driver_tm_g_pp_adverse_events()
     input_id <- "action-dataset_ADAE_singleextract-select"
     app_driver$set_active_module_input(input_id, "")
@@ -272,7 +273,7 @@ testthat::test_that(
 testthat::test_that(
   "e2e tm_g_pp_adverse_events - Selecting time column doesn't throw errors and changes a plot",
   {
-    # skip_if_too_deep(5)
+    skip_if_too_deep(5)
     # not possible for given app due to single choice
   }
 )
@@ -280,7 +281,7 @@ testthat::test_that(
 testthat::test_that(
   "e2e tm_g_pp_adverse_events - Deselecting time column throw validation error",
   {
-    # skip_if_too_deep(5)
+    skip_if_too_deep(5)
     app_driver <- app_driver_tm_g_pp_adverse_events()
     input_id <- "time-dataset_ADAE_singleextract-select"
     app_driver$set_active_module_input(input_id, "")
