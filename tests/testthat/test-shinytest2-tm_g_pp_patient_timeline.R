c545753a5f0f9dae1c950f699aab51e2b8ca14e0
unknown <kirar.kartikeya1@gmail.com>
2024-05-09 21:04:50 +0530

    fixing tm_g_pp_timeline

--- tests/testthat/test-shinytest2-tm_g_pp_patient_timeline.R
+++ tests/testthat/test-shinytest2-tm_g_pp_patient_timeline.R
@@ -77,7 +77,6 @@ app_driver_tm_g_pp_patient_timeline <- function() { # nolint object_length
         selected = "AENDY"
       ),
       font_size = c(12L, 12L, 25L),
-      plot_height = c(700L, 200L, 2000L),
       plot_width = NULL,
       pre_output = NULL,
       post_output = NULL,
