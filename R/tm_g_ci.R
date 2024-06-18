e71a5f734bb78a5e96c8f3452ba6f9c6fe44afc5
m7pr <marcin.kosinski.mk1@roche.com>
2024-06-18 15:56:42 +0200

    lintr changes

--- R/tm_g_ci.R
+++ R/tm_g_ci.R
@@ -501,6 +501,8 @@ srv_g_ci <- function(id,
       teal.reporter::simple_reporter_srv("simple_reporter", reporter = reporter, card_fun = card_fun)
     }
     ###
-    log_shiny_input_changes(input, level = logger::TRACE, namespace = "teal.modules.clinical", excluded_patterns = "_width$")
+    log_shiny_input_changes(
+      input, level = logger::TRACE, namespace = "teal.modules.clinical", excluded_patterns = "_width$"
+    )
   })
 }
--- R/tm_g_pp_therapy.R
+++ R/tm_g_pp_therapy.R
@@ -719,6 +719,8 @@ srv_g_therapy <- function(id,
       teal.reporter::simple_reporter_srv("simple_reporter", reporter = reporter, card_fun = card_fun)
     }
     ###
-    log_shiny_input_changes(input, level = logger::TRACE, namespace = "teal.modules.clinical", excluded_patterns = "_width$")
+    log_shiny_input_changes(
+      input, level = logger::TRACE, namespace = "teal.modules.clinical", excluded_patterns = "_width$"
+    )
   })
 }
