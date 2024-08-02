13fedf8b64658ec7e2ea87521bf0caad2c8a25b8
Emily de la Rua <emily.de_la_rua@contractors.roche.com>
2024-08-02 17:59:32 -0400

    Add tests

--- tests/testthat/test-tm_g_forest_rsp.R
+++ tests/testthat/test-tm_g_forest_rsp.R
@@ -15,3 +15,23 @@ testthat::test_that("template_forest_rsp generates correct expressions", {
   res <- testthat::expect_silent(result)
   testthat::expect_snapshot(res)
 })
+
+testthat::test_that("template_forest_rsp works with risk difference column added", {
+  result <- template_forest_rsp(
+    dataname = "adrs",
+    parentname = "adsl",
+    arm_var = "ARMCD",
+    ref_arm = "ARM A",
+    comp_arm = c("ARM B", "ARM C"),
+    aval_var = "AVALC",
+    responders = c("CR", "PR"),
+    subgroup_var = c("SEX", "STRATA2"),
+    strata_var = NULL,
+    stats = c("n_tot", "or", "ci"),
+    riskdiff = control_riskdiff(col_label = "Prop. Diff"),
+    conf_level = 0.95
+  )
+
+  res <- testthat::expect_silent(result)
+  testthat::expect_snapshot(res)
+})
--- tests/testthat/test-tm_g_forest_tte.R
+++ tests/testthat/test-tm_g_forest_tte.R
@@ -13,3 +13,21 @@ testthat::test_that("template_forest_tte generates correct expressions", {
   res <- testthat::expect_silent(result)
   testthat::expect_snapshot(res)
 })
+
+testthat::test_that("template_forest_tte works with risk difference column added", {
+  result <- template_forest_tte(
+    dataname = "adtte",
+    arm_var = "ARMCD",
+    ref_arm = "ARM A",
+    comp_arm = c("ARM B", "ARM C"),
+    subgroup_var = c("SEX", "BMRKR2"),
+    strata_var = "STRATA2",
+    stats = c("n_tot", "hr", "ci"),
+    riskdiff = control_riskdiff(col_label = "Prop. Diff"),
+    conf_level = 0.90,
+    col_symbol_size = NULL
+  )
+
+  res <- testthat::expect_silent(result)
+  testthat::expect_snapshot(res)
+})
