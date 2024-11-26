b891cb0f3d942741f8e187d61e388912a1a2e70e
m7pr <marcin.kosinski.mk1@roche.com>
2024-11-26 23:04:06 +0100

    introduce decorators for tm_t_ancova

--- R/tm_t_ancova.R
+++ R/tm_t_ancova.R
@@ -401,8 +401,7 @@ template_ancova <- function(dataname = "ANL",
   # Build table.
   y$table <- substitute(
     expr = {
-      result <- rtables::build_table(lyt = lyt, df = anl, alt_counts_df = parent)
-      result
+      table <- rtables::build_table(lyt = lyt, df = anl, alt_counts_df = parent)
     },
     env = list(
       anl = as.name(dataname),
@@ -424,6 +423,14 @@ template_ancova <- function(dataname = "ANL",
 #' @inheritParams template_ancova
 #'
 #' @inherit module_arguments return
+#'
+#' @section Decorating `tm_t_ancova`:
+#'
+#' This module generates the following objects, which can be modified in place using decorators:
+#' - `table` (`ElementaryTable` - output of `rtables::build_table`)
+#'
+#' For additional details and examples of decorators, refer to the vignette
+#' `vignette("decorate-modules-output", package = "teal")` or the [`teal_transform_module()`] documentation.
 #'
 #' @details
 #' When a single endpoint is selected, both unadjusted and adjusted comparison are provided. This modules
@@ -520,7 +527,8 @@ tm_t_ancova <- function(label,
                         conf_level = teal.transform::choices_selected(c(0.95, 0.9, 0.8), 0.95, keep_order = TRUE),
                         pre_output = NULL,
                         post_output = NULL,
-                        basic_table_args = teal.widgets::basic_table_args()) {
+                        basic_table_args = teal.widgets::basic_table_args(),
+                        decorators = NULL) {
   message("Initializing tm_t_ancova")
   checkmate::assert_string(label)
   checkmate::assert_string(dataname)
@@ -534,6 +542,14 @@ tm_t_ancova <- function(label,
   checkmate::assert_class(pre_output, classes = "shiny.tag", null.ok = TRUE)
   checkmate::assert_class(post_output, classes = "shiny.tag", null.ok = TRUE)
   checkmate::assert_class(basic_table_args, "basic_table_args")
+  if (checkmate::test_list(decorators, "teal_transform_module", null.ok = TRUE)) {
+    decorators <- if (checkmate::test_names(names(decorators), subset.of = c("default", "table"))) {
+      lapply(decorators, list)
+    } else {
+      list(default = decorators)
+    }
+  }
+  assert_decorators(decorators, null.ok = TRUE, names = c("default", "table"))
 
   args <- c(as.list(environment()))
 
@@ -566,7 +582,8 @@ tm_t_ancova <- function(label,
         arm_ref_comp = arm_ref_comp,
         include_interact = include_interact,
         label = label,
-        basic_table_args = basic_table_args
+        basic_table_args = basic_table_args,
+        decorators = decorators
       )
     ),
     datanames = teal.transform::get_extract_datanames(data_extract_list)
@@ -666,7 +683,8 @@ ui_ancova <- function(id, ...) {
               fixed = FALSE
             )
           )
-        )
+        ),
+        ui_decorate_teal_data(ns("decorator"), decorators = subset_decorators("table", a$decorators))
       )
     ),
     forms = tagList(
@@ -693,7 +711,8 @@ srv_ancova <- function(id,
                        paramcd,
                        avisit,
                        label,
-                       basic_table_args) {
+                       basic_table_args,
+                       decorators) {
   with_reporter <- !missing(reporter) && inherits(reporter, "Reporter")
   with_filter <- !missing(filter_panel_api) && inherits(filter_panel_api, "FilterPanelAPI")
   checkmate::assert_class(data, "reactive")
@@ -940,9 +959,16 @@ srv_ancova <- function(id,
       teal.code::eval_code(merged$anl_q(), as.expression(unlist(my_calls)))
     })
 
+    decorated_table_q <- srv_decorate_teal_data(
+      id = "decorator",
+      data = table_q,
+      decorators = subset_decorators("table", decorators),
+      expr = table
+    )
+
     # Output to render.
     table_r <- reactive({
-      table_q()[["result"]]
+      decorated_table_q()[["table"]]
     })
 
     teal.widgets::table_with_settings_srv(
@@ -953,7 +979,7 @@ srv_ancova <- function(id,
     # Render R code.
     teal.widgets::verbatim_popup_srv(
       id = "rcode",
-      verbatim_content = reactive(teal.code::get_code(table_q())),
+      verbatim_content = reactive(teal.code::get_code(req(decorated_table_q()))),
       title = label
     )
 
@@ -973,7 +999,7 @@ srv_ancova <- function(id,
           card$append_text("Comment", "header3")
           card$append_text(comment)
         }
-        card$append_src(teal.code::get_code(table_q()))
+        card$append_src(teal.code::get_code(req(decorated_table_q())))
         card
       }
       teal.reporter::simple_reporter_srv("simple_reporter", reporter = reporter, card_fun = card_fun)
