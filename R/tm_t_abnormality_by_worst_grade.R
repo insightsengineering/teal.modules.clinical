32537d9000cdf99eb51c6b53659b79bdfb119891
m7pr <marcin.kosinski.mk1@roche.com>
2024-11-27 10:33:17 +0100

    edit reporter part

--- R/tm_t_abnormality_by_worst_grade.R
+++ R/tm_t_abnormality_by_worst_grade.R
@@ -717,7 +717,7 @@ srv_t_abnormality_by_worst_grade <- function(id, # nolint: object_length.
           filter_panel_api = filter_panel_api
         )
         card$append_text("Table", "header3")
-        card$append_table(table_r())
+        card$append_table(req(decorated_table_q())))
         if (!comment == "") {
           card$append_text("Comment", "header3")
           card$append_text(comment)
