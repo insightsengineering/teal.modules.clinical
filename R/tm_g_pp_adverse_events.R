fce5e03d58395bb3c1037e21f6589955c7fa11e1
André Veríssimo <211358+averissimo@users.noreply.github.com>
2024-12-05 16:14:15 +0000

    chore: cleanup

--- R/tm_g_pp_adverse_events.R
+++ R/tm_g_pp_adverse_events.R
@@ -52,15 +52,12 @@ template_adverse_events <- function(dataname = "ANL",
             aeterm, tox_grade, causality, outcome, action, time, decod
           ) %>%
           dplyr::arrange(dplyr::desc(tox_grade)) %>%
-          `colnames<-`(col_labels(dataname, fill = TRUE)[vars])
-
-        # Exception for columns of type difftime that is not supported by as_listing
-        table <- dplyr::mutate(
-          table,
-          dplyr::across(
-            dplyr::where(~ inherits(., what = "difftime")), ~ as.double(., units = "auto")
+          `colnames<-`(col_labels(dataname, fill = TRUE)[vars]) %>%
+          dplyr::mutate( # Exception for columns of type difftime that is not supported by as_listing
+            dplyr::across(
+              dplyr::where(~ inherits(., what = "difftime")), ~ as.double(., units = "auto")
+            )
           )
-        )
         table <- rlistings::as_listing(
           table,
           key_cols = NULL,
