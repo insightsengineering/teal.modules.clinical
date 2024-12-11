06002486020cee065ec347695572be855889fe3f
Lluís Revilla <185338939+llrs-roche@users.noreply.github.com>
2024-12-11 09:58:21 +0100

    Apply suggestions from code review
    Co-authored-by: André Veríssimo <211358+averissimo@users.noreply.github.com>
    Signed-off-by: Lluís Revilla <185338939+llrs-roche@users.noreply.github.com>

--- R/tm_t_exposure.R
+++ R/tm_t_exposure.R
@@ -660,9 +660,10 @@ srv_t_exposure <- function(id,
     )
 
     # Render R code.
+    source_code_r <- reactive(teal.code::get_code(req(decorated_table_q())))
     teal.widgets::verbatim_popup_srv(
       id = "rcode",
-      verbatim_content = reactive(teal.code::get_code(req(decorated_table_q()))),
+      verbatim_content = source_code_r,
       title = label
     )
 
@@ -681,7 +682,7 @@ srv_t_exposure <- function(id,
           card$append_text("Comment", "header3")
           card$append_text(comment)
         }
-        card$append_src(teal.code::get_code(req(decorated_table_q())))
+        card$append_src(source_code_r())
         card
       }
       teal.reporter::simple_reporter_srv("simple_reporter", reporter = reporter, card_fun = card_fun)
