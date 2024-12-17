3c2bee36f0fce9656960935626d1103724004a6b
André Veríssimo <211358+averissimo@users.noreply.github.com>
2024-12-17 10:36:49 +0000

    fix: adds missing testing depth check

--- tests/testthat/test-examples.R
+++ tests/testthat/test-examples.R
@@ -124,6 +124,7 @@ for (i in rd_files()) {
     paste0("example-", basename(i)),
     {
       testthat::skip_on_cran()
+      skip_if_too_deep(5)
       if (basename(i) %in% strict_exceptions) {
         op <- options()
         withr::local_options(opts_partial_match_old)
