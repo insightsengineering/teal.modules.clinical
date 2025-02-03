c181d9839116720e4f84d0b0f698a94b6aefb305
Lluís Revilla <lluis.revilla_sancho@roche.com>
2025-02-03 15:46:01 +0100

    Remove modification to auxiliary function

--- R/arm_ref_comp.R
+++ R/arm_ref_comp.R
@@ -46,7 +46,6 @@ arm_ref_comp_observer <- function(session,
   iv$add_validator(iv1)
   iv$add_validator(iv2)
 
-  message("random code modified")
   output[[output_id]] <- renderUI({
     if (isTRUE(on_off())) {
       df <- if (is.reactive(data)) {
