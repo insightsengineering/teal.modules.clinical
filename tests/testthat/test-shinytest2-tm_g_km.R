b7eb2a8cd5770335bb2ce8510e2880681f8910c5
Emily de la Rua <emily.de_la_rua@contractors.roche.com>
2025-03-07 19:02:54 -0500

    Re-add tests

--- tests/testthat/test-shinytest2-tm_g_km.R
+++ tests/testthat/test-shinytest2-tm_g_km.R
@@ -61,7 +61,7 @@ app_driver_tm_g_km <- function() {
         fixed = TRUE
       ),
       conf_level = teal.transform::choices_selected(c(0.95, 0.9, 0.8, -1), 0.95, keep_order = TRUE),
-      conf_type = teal.transform::choices_selected(c("plain", "log", "log-log"), "log", keep_order = TRUE),
+      conf_type = teal.transform::choices_selected(c("plain", "log", "log-log", "none"), "log", keep_order = TRUE),
       font_size = c(11L, 1L, 30),
       control_annot_surv_med = control_surv_med_annot(),
       control_annot_coxph = control_coxph_annot(x = 0.27, y = 0.35, w = 0.3),
@@ -332,6 +332,7 @@ testthat::test_that("e2e - tm_g_km: Starts with specified collapsed additional p
   testthat::expect_false(app_driver$get_active_module_input("show_ci_ribbon"))
   testthat::expect_true(app_driver$get_active_module_input("show_km_table"))
   testthat::expect_equal(app_driver$get_active_module_input("conf_level"), "0.95")
+  testthat::expect_equal(app_driver$get_active_module_input("conf_type"), "log")
   testthat::expect_equal(app_driver$get_active_module_input("xlab"), "Time")
 
   testthat::expect_equal(
@@ -371,6 +372,7 @@ test_that_plot_settings("rel_height_plot", 70)
 test_that_plot_settings("show_ci_ribbon", TRUE)
 test_that_plot_settings("show_km_table", FALSE)
 test_that_plot_settings("conf_level", 0.8)
+test_that_plot_settings("conf_type", "plain")
 test_that_plot_settings("xlab", "Time2")
 
 testthat::test_that("e2e - tm_g_km: Deselecting {conf_level} throws validation error.", {
@@ -381,6 +383,18 @@ testthat::test_that("e2e - tm_g_km: Deselecting {conf_level} throws validation e
   testthat::expect_match(
     app_driver$active_module_element_text("myplot-plot-with-settings"),
     "Confidence level must be between 0 and 1."
+  )
+  app_driver$stop()
+})
+
+testthat::test_that("e2e - tm_g_km: Deselecting {conf_type} throws validation error.", {
+  skip_if_too_deep(5)
+  app_driver <- app_driver_tm_g_km()
+  app_driver$set_active_module_input("conf_type", "none")
+  app_driver$expect_validation_error()
+  testthat::expect_match(
+    app_driver$active_module_element_text("myplot-plot-with-settings"),
+    "Confidence interval type must be one of plain, log, log-log."
   )
   app_driver$stop()
 })
