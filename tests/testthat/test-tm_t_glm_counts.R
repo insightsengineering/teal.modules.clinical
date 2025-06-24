63934d8bbdb4afc86d7aaeb82f0b5ac404009c2e
Lluís Revilla <185338939+llrs-roche@users.noreply.github.com>
2025-06-24 15:22:28 +0200

    Adding new module using `summarize_glm_count` (#1386)
    # Pull Request
    
    Fixes #1379
    
    This PR adds a new module `tm_t_glm_counts` that uses
    `tern::summarize_glm_count` to create a table output per request.
    
    
    <details><summary>Sample app on how to use it </summary>
    <p>
    
    ```r
    devtools::load_all("../teal.modules.clinical")
    data <- within(teal_data(), {
      library("tern")
      ADSL <- tern_ex_adsl
      ADTTE <- tern_ex_adtte
    })
    
    join_keys(data) <- default_cdisc_join_keys[names(data)]
    
    arm_ref_comp <- list(
      ACTARMCD = list(
        ref = "ARM B",
        comp = c("ARM A", "ARM C")
      ),
      ARM = list(
        ref = "B: Placebo",
        comp = c("A: Drug X", "C: Combination")
      )
    )
    
    ADSL <- data[["ADSL"]]
    ADTTE <- data[["ADTTE"]]
    # Initialize the teal app
    app <- init(
      data = data,
      modules = modules(
        tm_t_glm_counts(
          dataname = "ADTTE",
          arm_var = choices_selected(
            variable_choices(ADTTE, c("ARM", "ARMCD", "ACTARMCD")),
            "ARMCD"
          ),
          arm_ref_comp = arm_ref_comp,
          aval_var = choices_selected(
            variable_choices(ADTTE, "AVAL"),
            "AVAL"
          ),
          strata_var = choices_selected(
            variable_choices(ADSL, "SEX"),
            NULL
          ),
          offset_var = choices_selected(
            variable_choices(ADSL, "AGE"),
            NULL
          ),
          cov_var = choices_selected(
            variable_choices(ADTTE, "SITEID"),
            NULL
          )
        )
      )
    )
    
    if (interactive()) {
      shinyApp(ui = app$ui, server = app$server)
    }
    
    ``` 
    
    
    </p>
    </details> 
    
    Image of the output
    
    
    ![image](https://github.com/user-attachments/assets/2906bde6-5c64-4022-86b1-a994619326fd)
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ tests/testthat/test-tm_t_glm_counts.R
@@ -0,0 +1,84 @@
+app_driver_tm_t_glm_counts <- function() { # nolint: object_length.
+  data <- within(teal_data(), {
+    library("tern")
+    ADSL <- tern_ex_adsl
+    ADTTE <- tern_ex_adtte
+  })
+
+  join_keys(data) <- default_cdisc_join_keys[names(data)]
+
+  arm_ref_comp <- list(
+    ACTARMCD = list(
+      ref = "ARM B",
+      comp = c("ARM A", "ARM C")
+    ),
+    ARM = list(
+      ref = "B: Placebo",
+      comp = c("A: Drug X", "C: Combination")
+    )
+  )
+
+  ADSL <- data[["ADSL"]]
+  ADTTE <- data[["ADTTE"]]
+  # Initialize the teal app
+  init_teal_app_driver(
+    data = data,
+    modules = tm_t_glm_counts(
+      dataname = "ADTTE",
+      arm_var = choices_selected(
+        variable_choices("ADTTE", c("ARM", "ARMCD", "ACTARMCD")),
+        "ARMCD"
+      ),
+      arm_ref_comp = arm_ref_comp,
+      aval_var = choices_selected(
+        variable_choices("ADTTE", "AVAL"),
+        "AVAL"
+      ),
+      strata_var = choices_selected(
+        variable_choices("ADSL", "SEX"),
+        NULL
+      ),
+      offset_var = choices_selected(
+        variable_choices("ADSL", "AGE"),
+        NULL
+      ),
+      cov_var = choices_selected(
+        variable_choices("ADTTE", "SITEID"),
+        NULL
+      )
+    )
+  )
+}
+
+testthat::test_that(
+  "e2e - tm_t_glm_counts: Module initializes in teal without errors and produces table output.",
+  {
+    skip_if_too_deep(5)
+    app_driver <- app_driver_tm_t_glm_counts()
+    app_driver$expect_no_shiny_error()
+    app_driver$expect_no_validation_error()
+    testthat::expect_true(
+      app_driver$is_visible(app_driver$active_module_element("table-table-with-settings"))
+    )
+    app_driver$stop()
+  }
+)
+
+
+testthat::test_that(
+  "e2e - tm_t_events_by_grade: Selecting arm_var changes the table and does not throw validation errors.",
+  {
+    skip_if_too_deep(5)
+    app_driver <- app_driver_tm_t_glm_counts()
+    table_before <- app_driver$get_active_module_table_output("table-table-with-settings")
+    app_driver$set_active_module_input("arm_var-dataset_ADSL_singleextract-select", "ACTARMCD")
+    testthat::expect_false(
+      identical(
+        table_before,
+        app_driver$get_active_module_table_output("table-table-with-settings")
+      )
+    )
+    app_driver$expect_no_validation_error()
+    app_driver$stop()
+  }
+)
