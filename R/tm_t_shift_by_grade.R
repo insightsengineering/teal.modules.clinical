dd9a49df406426a5c03c4ef7ba3f58ee55a04296
m7pr <marcin.kosinski.mk1@roche.com>
2025-09-22 13:36:23 +0200

    remove ANL print in t_shift_by_grade

--- R/tm_t_shift_by_grade.R
+++ R/tm_t_shift_by_grade.R
@@ -273,7 +273,6 @@ template_shift_by_grade <- function(parentname,
           BTOXGR_GP = "Baseline Grade"
         )
         teal.data::col_labels(dataname)[names(column_labels)] <- as.character(column_labels)
-        dataname
       },
       env = list(
         dataname = as.name("anl"),
