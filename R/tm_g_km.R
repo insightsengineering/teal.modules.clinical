4167870aacd8b1aeb49caecbe24cde8ce9d2d210
André Veríssimo <211358+averissimo@users.noreply.github.com>
2025-09-29 14:44:50 +0100

    fix: remove add to reporter button

--- R/tm_g_km.R
+++ R/tm_g_km.R
@@ -467,10 +467,6 @@ ui_g_km <- function(id, ...) {
       )
     ),
     encoding = tags$div(
-      ### Reporter
-      teal.reporter::add_card_button_ui(ns("add_reporter"), label = "Add Report Card"),
-      tags$br(), tags$br(),
-      ###
       tags$label("Encodings", class = "text-primary"), tags$br(),
       teal.transform::datanames_input(a[c("arm_var", "paramcd", "strata_var", "facet_var", "aval_var", "cnsr_var")]),
       teal.transform::data_extract_ui(
