c949ea1388746dc315b349b559b70be9f5a294bc
Lluís Revilla <lluis.revilla_sancho@roche.com>
2025-11-24 17:03:48 +0100

    Update id for tm_t_summary_by

--- tests/testthat/test-shinytest2-tm_t_summary_by.R
+++ tests/testthat/test-shinytest2-tm_t_summary_by.R
@@ -71,7 +71,7 @@ testthat::test_that(
     skip_if_too_deep(5)
     app_driver <- app_driver_tm_t_summary_by()
     testthat::expect_equal(
-      app_driver$get_text("#teal-teal_modules-active_tab .active > a"),
+      app_driver$get_text("#teal-teal_modules-active_module_id * .teal-modules-tree * .nav-link"),
       "Summary by Row Groups Table"
     )
     testthat::expect_equal(
