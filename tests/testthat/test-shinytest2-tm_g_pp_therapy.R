d9100ba84d22108d51cdbc025b8056c7072d0c4d
osenan <oriol@praenoscere.com>
2025-11-26 14:52:32 -0300

    test: use more neutral selector

--- tests/testthat/test-shinytest2-tm_g_pp_therapy.R
+++ tests/testthat/test-shinytest2-tm_g_pp_therapy.R
@@ -113,7 +113,7 @@ testthat::test_that(
     app_driver <- app_driver_tm_g_pp_therapy()
 
     testthat::expect_equal(
-      trimws(app_driver$get_text(".teal-modules-wrapper .module-button.active")),
+      trimws(app_driver$get_text("a.nav-link.active")),
       "Therapy (e2e)"
     )
 
