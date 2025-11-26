a3500a81787ba9fe2da78834705c1cad71627af9
m7pr <marcin.kosinski.mk1@roche.com>
2025-11-26 15:14:03 +0100

    fix links

--- tests/testthat/test-shinytest2-tm_t_pp_prior_medication.R
+++ tests/testthat/test-shinytest2-tm_t_pp_prior_medication.R
@@ -63,7 +63,7 @@ testthat::test_that(
     skip_if_too_deep(5)
     app_driver <- app_driver_tm_t_pp_prior_medication()
     testthat::expect_equal(
-      app_driver$get_text("#teal-teal_modules-active_tab .active > a"),
+      app_driver$get_text("a.nav-link.active"),
       "Prior Medication"
     )
     testthat::expect_equal(
