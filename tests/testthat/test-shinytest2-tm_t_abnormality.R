b03a843f5516f77a9ec4fed50238bed5d5a07c02
m7pr <marcin.kosinski.mk1@roche.com>
2025-11-26 11:56:24 +0100

    fix abnormality

--- tests/testthat/test-shinytest2-tm_t_abnormality.R
+++ tests/testthat/test-shinytest2-tm_t_abnormality.R
@@ -82,7 +82,7 @@ testthat::test_that(
     app_driver <- app_driver_tm_t_abnormality()
 
     testthat::expect_equal(
-      app_driver$get_text("#teal-teal_modules-active_tab .active > a"),
+      app_driver$get_text("a.nav-link.active"),
       "Abnormality Table"
     )
     testthat::expect_equal(
