701c5fa0967508f051f11f0b74c1f00ac2f4d128
m7pr <marcin.kosinski.mk1@roche.com>
2025-11-26 14:14:38 +0100

    fix links

--- tests/testthat/test-shinytest2-tm_t_pp_laboratory.R
+++ tests/testthat/test-shinytest2-tm_t_pp_laboratory.R
@@ -64,7 +64,7 @@ testthat::test_that(
     app_driver <- app_driver_tm_t_pp_laboratory()
 
     testthat::expect_equal(
-      app_driver$get_text("#teal-teal_modules-active_tab .active > a"),
+      app_driver$get_text("a.nav-link.active"),
       "Vitals"
     )
     testthat::expect_equal(
