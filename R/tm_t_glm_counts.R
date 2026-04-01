5bfbdea396c023bcd5372d4188ac7f2bc5bf13e3
m7pr <marcin.kosinski.mk1@roche.com>
2026-04-01 14:45:52 +0200

    5 more modules

--- R/tm_t_glm_counts.R
+++ R/tm_t_glm_counts.R
@@ -6,8 +6,14 @@
 #' @inheritParams template_arguments
 #' @inheritParams module_arguments
 #' @inheritParams teal::module
+#' @param arm_var ([teal.picks::variables()])\cr treatment variable (`parentname`).
+#' @param aval_var ([teal.picks::variables()])\cr analysis variable (`dataname`).
+#' @param strata_var ([teal.picks::variables()])\cr optional stratification columns (`dataname`).
+#' @param offset_var ([teal.picks::variables()])\cr optional offset column (`dataname`).
+#' @param cov_var ([teal.picks::variables()])\cr optional covariate columns (`dataname`).
 #' @param conf_level ([teal.transform::choices_selected()])\cr object with all available choices and
 #'   pre-selected option for confidence level, each within range of (0, 1).
+#' @param add_total (`logical`)\cr initial value for the \dQuote{Add All Patients column} checkbox when comparing arms.
 #' @param rate_mean_method (`character`) method used to estimate the mean odds ratio. Either "emmeans" or "ppmeans"
 #' (as in `summarize_glm_count()`).
 #' @param distribution (`character`) value specifying the distribution used in the regression model
@@ -77,27 +83,15 @@
 #'   modules = modules(
 #'     tm_t_glm_counts(
 #'       dataname = "ADTTE",
-#'       arm_var = choices_selected(
-#'         variable_choices(ADTTE, c("ARM", "ARMCD", "ACTARMCD")),
-#'         "ARMCD"
+#'       arm_var = variables(
+#'         choices = any_of(c("ARM", "ARMCD", "ACTARMCD")),
+#'         selected = "ARMCD"
 #'       ),
 #'       arm_ref_comp = arm_ref_comp,
-#'       aval_var = choices_selected(
-#'         variable_choices(ADTTE, "AVAL"),
-#'         "AVAL"
-#'       ),
-#'       strata_var = choices_selected(
-#'         variable_choices(ADSL, "SEX"),
-#'         NULL
-#'       ),
-#'       offset_var = choices_selected(
-#'         variable_choices(ADSL, "AGE"),
-#'         NULL
-#'       ),
-#'       cov_var = choices_selected(
-#'         variable_choices(ADTTE, "SITEID"),
-#'         NULL
-#'       )
+#'       aval_var = variables(choices = any_of("AVAL"), selected = "AVAL", fixed = TRUE),
+#'       strata_var = variables(choices = any_of("SEX"), selected = NULL),
+#'       offset_var = variables(choices = any_of("AGE"), selected = NULL),
+#'       cov_var = variables(choices = any_of("SITEID"), selected = NULL)
 #'     )
 #'   )
 #' )
@@ -108,15 +102,8 @@
 #' @export
 tm_t_glm_counts <- function(label = "Counts Module",
                             dataname,
-                            parentname = ifelse(
-                              inherits(arm_var, "data_extract_spec"),
-                              teal.transform::datanames_input(arm_var),
-                              "ADSL"
-                            ),
-                            aval_var = teal.transform::choices_selected(
-                              teal.transform::variable_choices(dataname, "AVAL"), "AVAL",
-                              fixed = TRUE
-                            ),
+                            parentname = "ADSL",
+                            aval_var = variables(choices = any_of("AVAL"), selected = "AVAL", fixed = TRUE),
                             arm_var,
                             strata_var,
                             rate_mean_method = c("emmeans", "ppmeans"),
@@ -125,6 +112,7 @@ tm_t_glm_counts <- function(label = "Counts Module",
                             cov_var,
                             arm_ref_comp = NULL,
                             conf_level = teal.transform::choices_selected(c(0.95, 0.9, 0.8), 0.95, keep_order = TRUE),
+                            add_total = FALSE,
                             pre_output = NULL,
                             post_output = NULL,
                             basic_table_args = teal.widgets::basic_table_args(),
@@ -133,57 +121,59 @@ tm_t_glm_counts <- function(label = "Counts Module",
   message("Initializing tm_t_glm_counts")
   checkmate::assert_string(label)
   checkmate::assert_string(dataname)
-  rate_mean_method <- match.arg(rate_mean_method)
-  distribution <- match.arg(distribution)
+  distribution_choices <- c("negbin", "quasipoisson", "poisson")
+  rate_mean_method_choices <- c("emmeans", "ppmeans")
+  rate_mean_method <- match.arg(rate_mean_method, rate_mean_method_choices)
+  distribution <- match.arg(distribution, distribution_choices)
   checkmate::assert_string(parentname)
-  checkmate::assert_class(arm_var, "choices_selected")
+  checkmate::assert_class(arm_var, "variables")
+  checkmate::assert_class(aval_var, "variables")
+  checkmate::assert_class(strata_var, "variables")
+  checkmate::assert_class(offset_var, "variables")
+  checkmate::assert_class(cov_var, "variables")
+  checkmate::assert_class(conf_level, "choices_selected")
+  checkmate::assert_flag(add_total)
   checkmate::assert_class(pre_output, classes = "shiny.tag", null.ok = TRUE)
   checkmate::assert_class(post_output, classes = "shiny.tag", null.ok = TRUE)
   checkmate::assert_class(basic_table_args, "basic_table_args")
   assert_decorators(decorators, "table")
 
-  args <- as.list(environment())
+  arm_var <- picks(datasets(parentname, parentname), arm_var)
+  aval_var <- picks(datasets(dataname, dataname), aval_var)
+  cov_var <- picks(datasets(dataname, dataname), cov_var)
+  offset_var <- picks(datasets(dataname, dataname), offset_var)
+  strata_var <- picks(datasets(dataname, dataname), strata_var)
 
-  data_extract_list <- list(
-    arm_var = cs_to_des_select(arm_var, dataname = parentname),
-    aval_var = cs_to_des_select(aval_var, dataname = dataname),
-    cov_var = cs_to_des_select(cov_var, dataname = dataname),
-    offset_var = cs_to_des_select(offset_var, dataname = dataname),
-    strata_var = cs_to_des_select(strata_var, dataname = dataname)
-  )
+  args <- as.list(environment())
 
   teal::module(
     label = label,
     ui = ui_t_glm_counts,
     server = srv_t_glm_counts,
-    ui_args = c(data_extract_list, args),
-    server_args = c(
-      data_extract_list,
-      list(
-        dataname = dataname,
-        parentname = parentname,
-        arm_ref_comp = arm_ref_comp,
-        label = label,
-        basic_table_args = basic_table_args,
-        decorators = decorators
-      )
-    ),
+    ui_args = args[names(args) %in% names(formals(ui_t_glm_counts))],
+    server_args = args[names(args) %in% names(formals(srv_t_glm_counts))],
     transformators = transformators,
-    datanames = teal.transform::get_extract_datanames(data_extract_list)
+    datanames = union(parentname, dataname)
   )
 }
 
 
-ui_t_glm_counts <- function(id, ...) {
+ui_t_glm_counts <- function(id,
+                            arm_var,
+                            aval_var,
+                            cov_var,
+                            offset_var,
+                            strata_var,
+                            distribution_choices,
+                            distribution,
+                            rate_mean_method_choices,
+                            rate_mean_method,
+                            conf_level,
+                            add_total,
+                            pre_output,
+                            post_output,
+                            decorators) {
   ns <- NS(id)
-  a <- list(...) # module args
-
-  is_single_dataset_value <- teal.transform::is_single_dataset(
-    a$arm_var,
-    a$offset_var,
-    a$cov_var,
-    a$aval_var
-  )
   output <- teal.widgets::white_small_well(
     teal.widgets::table_with_settings_ui(ns("table"))
   )
@@ -194,35 +184,35 @@ ui_t_glm_counts <- function(id, ...) {
     bslib::input_switch(
       id = ns("compare_arms"),
       label = "Compare Treatments",
-      value = !is.null(a$arm_var)
+      value = TRUE
     ),
     conditionalPanel(
       condition = paste0("input['", ns("compare_arms"), "']"),
       tags$div(
-        uiOutput(ns("arms_buckets")), # from arm_ref_comp_observer
-        uiOutput(ns("helptext_ui")), # For feedback on comparisons
+        uiOutput(ns("arms_buckets")),
+        uiOutput(ns("helptext_ui")),
         checkboxInput(
           ns("combine_comp_arms"),
           "Combine all comparison groups?",
           value = FALSE
         ),
-        checkboxInput(ns("add_total"), "Add All Patients column", value = a$add_total)
+        checkboxInput(ns("add_total"), "Add All Patients column", value = add_total)
       )
     )
   )
 
   table_settings <- shiny::tagList(
-    ui_decorate_teal_data(ns("decorator"), decorators = select_decorators(a$decorators, "table")),
+    ui_decorate_teal_data(ns("decorator"), decorators = select_decorators(decorators, "table")),
     bslib::accordion(
       bslib::accordion_panel(
         "Additional table settings",
         teal.widgets::optionalSelectInput(
           inputId = ns("conf_level"),
           label = "Confidence Level",
-          choices = c(0.8, 0.9, 0.95),
-          selected = 0.95,
+          choices = conf_level$choices,
+          selected = conf_level$selected,
           multiple = FALSE,
-          fixed = FALSE
+          fixed = conf_level$fixed
         ),
       )
     )
@@ -232,51 +222,28 @@ ui_t_glm_counts <- function(id, ...) {
     output = output,
     encoding = tags$div(
       tags$label("Encodings", class = "text-primary"), tags$br(),
-      teal.transform::data_extract_ui(
-        ns("arm_var"),
-        "Select Treatment Variable",
-        data_extract_spec = a$arm_var,
-        is_single_dataset = is_single_dataset_value
-      ),
-      teal.transform::data_extract_ui(
-        id = ns("aval_var"),
-        label = "Analysis Variable",
-        data_extract_spec = a$aval_var,
-        is_single_dataset = is_single_dataset_value
-      ),
-      teal.transform::data_extract_ui(
-        ns("cov_var"),
-        "Covariate(s)",
-        data_extract_spec = a$cov_var,
-        is_single_dataset = is_single_dataset_value
-      ),
-      teal.transform::data_extract_ui(
-        ns("offset_var"),
-        "Offset variable",
-        data_extract_spec = a$offset_var,
-        is_single_dataset = is_single_dataset_value
-      ),
-      teal.transform::data_extract_ui(
-        id = ns("strata_var"),
-        label = "Stratify by",
-        data_extract_spec = a$strata_var,
-        is_single_dataset = is_single_dataset_value
-      ),
+      tags$div(tags$label("Select Treatment Variable"), picks_ui(ns("arm_var"), arm_var)),
+      tags$div(tags$label("Analysis Variable"), picks_ui(ns("aval_var"), aval_var)),
+      tags$div(tags$label("Covariate(s)"), picks_ui(ns("cov_var"), cov_var)),
+      tags$div(tags$label("Offset variable"), picks_ui(ns("offset_var"), offset_var)),
+      tags$div(tags$label("Stratify by"), picks_ui(ns("strata_var"), strata_var)),
       compare_treatments,
       shiny::selectInput(
         ns("distribution"),
         "Distribution",
-        choices = a$distribution
+        choices = distribution_choices,
+        selected = distribution
       ),
       shiny::selectInput(
         ns("rate_mean_method"),
         "Rate method",
-        choices = a$rate_mean_method
+        choices = rate_mean_method_choices,
+        selected = rate_mean_method
       ),
       table_settings
     ),
-    pre_output = a$pre_output,
-    post_output = a$post_output
+    pre_output = pre_output,
+    post_output = post_output
   )
 }
 
@@ -298,99 +265,92 @@ srv_t_glm_counts <- function(id,
   moduleServer(id, function(input, output, session) {
     teal.logger::log_shiny_input_changes(input, namespace = "teal.modules.clinical")
 
-    # Input validation
     iv_arm_ref <- arm_ref_comp_observer(
       session,
       input,
       output,
-      id_arm_var = extract_input("arm_var", parentname),
+      id_arm_var = "arm_var-variables-selected",
       data = reactive(data()[[parentname]]),
       arm_ref_comp = arm_ref_comp,
       module = "tm_t_counts",
       on_off = reactive(input$compare_arms)
     )
 
-    list_data_extract <- list(
-      arm_var = arm_var,
-      aval_var = aval_var,
-      strata_var = strata_var,
-      cov_var = cov_var,
-      offset_var = offset_var
+    anl_selectors <- picks_srv(
+      id = "",
+      picks = list(
+        arm_var = arm_var,
+        aval_var = aval_var,
+        strata_var = strata_var,
+        cov_var = cov_var,
+        offset_var = offset_var
+      ),
+      data = data
     )
 
-    selector_list <- teal.transform::data_extract_multiple_srv(
-      data_extract = list_data_extract,
-      datasets = data,
-      select_validation_rule = list(
-        aval_var = shinyvalidate::sv_required("An analysis variable is required"),
-        arm_var = shinyvalidate::sv_required("A treatment variable is required")
-      )
-    )
-
-    ## Data source merging
-    adsl_merge_inputs <- teal.transform::merge_expression_module(
-      datasets = data,
-      data_extract = list_data_extract,
-      anl_name = "ANL"
-    )
-
-    output$helptext_ui <- renderUI({
-      req(selector_list()$arm_var()$select)
-      helpText("Multiple reference groups are automatically combined into a single group.")
-    })
-
-    iv_r <- reactive({
-      iv <- shinyvalidate::InputValidator$new()
-
-      if (isTRUE(input$compare_arms)) {
-        iv$add_validator(iv_arm_ref)
-      }
-
-      iv$add_rule("conf_level", shinyvalidate::sv_required("Please choose a confidence level"))
-      iv$add_rule(
-        "conf_level", shinyvalidate::sv_between(
-          0, 1,
-          message_fmt = "Confidence level must be between 0 and 1"
-        )
-      )
-      teal.transform::compose_and_enable_validators(iv, selector_list)
-    })
-
-    ## Merge data
-    anl_q <- reactive({
+    data_with_card <- reactive({
       obj <- data()
       teal.reporter::teal_card(obj) <-
         c(
           teal.reporter::teal_card(obj),
           teal.reporter::teal_card("## Module's output(s)")
         )
-      obj %>%
-        teal.code::eval_code(as.expression(adsl_merge_inputs()$expr))
+      obj
+    })
+    merged_anl <- merge_srv("merge_anl", data = data_with_card, selectors = anl_selectors, output_name = "ANL")
+    anl_q <- merged_anl$data
+
+    output$helptext_ui <- renderUI({
+      req(length(anl_selectors$arm_var()$variables$selected) > 0L)
+      helpText("Multiple reference groups are automatically combined into a single group.")
+    })
+
+    iv_r <- reactive({
+      iv <- shinyvalidate::InputValidator$new()
+      if (isTRUE(input$compare_arms)) {
+        iv$add_validator(iv_arm_ref)
+      }
+      iv$add_rule("conf_level", shinyvalidate::sv_required("Please choose a confidence level"))
+      iv$add_rule(
+        "conf_level",
+        shinyvalidate::sv_between(
+          0, 1,
+          message_fmt = "Confidence level must be between 0 and 1"
+        )
+      )
+      iv$enable()
+      iv
     })
 
     validate_checks <- reactive({
       teal::validate_inputs(iv_r())
+      validate(
+        need(
+          length(anl_selectors$arm_var()$variables$selected) >= 1L,
+          "A treatment variable is required"
+        ),
+        need(
+          length(anl_selectors$aval_var()$variables$selected) >= 1L,
+          "An analysis variable is required"
+        )
+      )
+
       adsl_filtered <- anl_q()[[parentname]]
       anl_filtered <- anl_q()[[dataname]]
       anl <- anl_q()[["ANL"]]
 
-      anl_m <- adsl_merge_inputs()
-      input_arm_var <- as.vector(anl_m$columns_source$arm_var)
-      input_strata_var <- as.vector(anl_m$columns_source$strata_var)
-      input_aval_var <- as.vector(anl_m$columns_source$aval_var)
+      input_arm_var <- as.vector(anl_selectors$arm_var()$variables$selected)
+      input_strata_var <- as.vector(anl_selectors$strata_var()$variables$selected)
+      input_aval_var <- as.vector(anl_selectors$aval_var()$variables$selected)
 
-      # validate inputs
       validate_args <- list(
         adsl = adsl_filtered,
         adslvars = c("USUBJID", "STUDYID", input_arm_var, input_strata_var),
         anl = anl_filtered,
-        anlvars = c(
-          "USUBJID", "STUDYID", input_aval_var # , input_paramcd
-        ),
+        anlvars = c("USUBJID", "STUDYID", input_aval_var),
         arm_var = input_arm_var
       )
 
-      # validate arm levels
       if (length(input_arm_var) > 0 && length(unique(adsl_filtered[[input_arm_var]])) == 1) {
         validate_args <- append(validate_args, list(min_n_levels_armvar = NULL))
       }
@@ -403,7 +363,6 @@ srv_t_glm_counts <- function(id,
 
       do.call(what = "validate_standard_inputs", validate_args)
 
-      # check that there is at least one record with no missing data
       validate(shiny::need(
         !all(is.na(anl[[input_aval_var]])),
         "ANCOVA table cannot be calculated as all values are missing."
@@ -412,33 +371,32 @@ srv_t_glm_counts <- function(id,
       NULL
     })
 
-    ##  Preprocessing the data: user specified
     anl <- reactive({
       within(req(anl_q()), {
         ANL <- tern::df_explicit_na(ANL)
       })
     })
-    ## Add basic specification for the table
+
     basic_table <- reactive({
+      validate_checks()
       req(!is.null(input$buckets$Ref))
-      ami <- req(adsl_merge_inputs())
+      req(anl_q())
+      arm_col <- as.vector(anl_selectors$arm_var()$variables$selected)
       within(req(anl()),
         {
           lyt <- rtables::basic_table(show_colcounts = TRUE) %>%
             rtables::split_cols_by(var, ref_group = ref_group, split_fun = tern::ref_group_position("first"))
         },
         ref_group = unlist(input$buckets$Ref),
-        var = as.vector(ami$columns_source$arm_var)
+        var = arm_col
       )
     })
 
-    ## Create covariates for the table
-    ## Create tables
     summarize_counts <- reactive({
-      ami <- req(adsl_merge_inputs())
-      offset_var <- as.vector(ami$columns_source$offset_var)
-      cov_var <- as.vector(ami$columns_source$cov_var)
-      arm_var <- as.vector(ami$columns_source$arm_var)
+      validate_checks()
+      offset_var <- as.vector(anl_selectors$offset_var()$variables$selected)
+      cov_var <- as.vector(anl_selectors$cov_var()$variables$selected)
+      arm_var <- as.vector(anl_selectors$arm_var()$variables$selected)
       variables <- if (length(offset_var) && length(cov_var)) {
         within(req(basic_table()),
           {
@@ -491,7 +449,7 @@ srv_t_glm_counts <- function(id,
           )
         },
         rate_mean_method = input$rate_mean_method,
-        var = as.vector(ami$columns_source$aval_var),
+        var = as.vector(anl_selectors$aval_var()$variables$selected),
         conf_level = as.numeric(input$conf_level),
         distribution = input$distribution
       )
