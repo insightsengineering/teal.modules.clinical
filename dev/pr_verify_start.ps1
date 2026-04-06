55798963235e290447e3ca4017a567b853e499bf
m7pr <marcin.kosinski.mk1@roche.com>
2026-04-06 14:05:21 +0200

    chore(pr): browser verification screenshot for tm_t_shift_by_grade

--- dev/pr_verify_start.ps1
+++ dev/pr_verify_start.ps1
@@ -11,5 +11,5 @@ Copy-Item -Force "C:\temp\tmc_pr_helpers\*" "dev\"
 New-Item -ItemType Directory -Force "inst\pr_verification" | Out-Null
 & "C:\Program Files\R\R-devel\bin\Rscript.exe" "dev\extract_rd_examples.R" $Topic | Out-File -Encoding utf8 "_topic_code.txt"
 Start-Process -FilePath "C:\Program Files\R\R-devel\bin\Rscript.exe" -ArgumentList @("dev/run_example_in_browser.R", $Topic, "$Port") -WindowStyle Hidden
-Start-Sleep -Seconds 34
-(Invoke-WebRequest -Uri "http://127.0.0.1:$Port/" -UseBasicParsing -TimeoutSec 10).StatusCode
+Start-Sleep -Seconds 45
+(Invoke-WebRequest -Uri "http://127.0.0.1:$Port/" -UseBasicParsing -TimeoutSec 90).StatusCode
