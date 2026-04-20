75cc80093ac2a5d2ffac381e003bc1683ff64000
André Veríssimo <211358+averissimo@users.noreply.github.com>
2026-04-20 12:11:19 +0100

    chore: missing defer

--- tests/testthat/test-shinytest2-tm_t_pp_laboratory.R
+++ tests/testthat/test-shinytest2-tm_t_pp_laboratory.R
@@ -51,6 +51,7 @@ app_driver_tm_t_pp_laboratory <- function() {
 testthat::test_that("e2e - tm_t_pp_laboratory: Module initializes in teal without errors and produces table output.", {
   skip_if_too_deep(5)
   app_driver <- app_driver_tm_t_pp_laboratory()
+  withr::defer(app_driver$stop())
   app_driver$wait_for_idle()
   app_driver$expect_no_shiny_error()
   app_driver$expect_no_validation_error()
