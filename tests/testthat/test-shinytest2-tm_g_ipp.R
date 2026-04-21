e73b26f1f0d1b6684b8a401e64e6de22b02f5d65
André Veríssimo <211358+averissimo@users.noreply.github.com>
2026-04-21 11:39:03 +0100

    wip

--- tests/testthat/test-shinytest2-tm_g_ipp.R
+++ tests/testthat/test-shinytest2-tm_g_ipp.R
@@ -2,15 +2,27 @@ app_driver_tm_g_ipp <- function() {
   data <- within(teal.data::teal_data(), {
     library(dplyr)
     library(tern)
-    ADSL <- teal.data::rADSL %>%
-      slice(1:20) %>%
+    ADSL <- teal.data::rADSL |>
+      slice(1:20) |>
       df_explicit_na()
-    ADLB <- teal.data::rADLB %>%
-      filter(USUBJID %in% ADSL$USUBJID) %>%
-      df_explicit_na() %>%
+    ADLB <- teal.data::rADLB |>
+      filter(USUBJID %in% ADSL$USUBJID) |>
+      df_explicit_na() |>
       filter(AVISIT != "SCREENING")
   })
   teal.data::join_keys(data) <- teal.data::default_cdisc_join_keys[names(data)]
+
+  testthat::expect_warning(
+    paramcd_value <- teal.picks::values(selected = "ALT", multiple = FALSE),
+    "doesn't guarantee that `selected` is a subset of `choices`.",
+    fixed = TRUE
+  )
+
+  testthat::expect_warning(
+    arm_var_value <- teal.picks::values(selected = "ARM A", multiple = FALSE),
+    "doesn't guarantee that `selected` is a subset of `choices`.",
+    fixed = TRUE
+  )
 
   init_teal_app_driver(
     teal::init(
@@ -19,37 +31,18 @@ app_driver_tm_g_ipp <- function() {
         label = "Individual Patient Plot",
         dataname = "ADLB",
         parentname = "ADSL",
-        arm_var = teal.transform::choices_selected(
-          teal.transform::value_choices(data[["ADLB"]], "ARMCD"),
-          "ARM A"
+        arm_var = teal.picks::picks(
+          teal.picks::datasets("ADSL"),
+          teal.picks::variables("ARMCD", fixed = TRUE),
+          arm_var_value
         ),
-        paramcd = teal.transform::choices_selected(
-          teal.transform::value_choices(data[["ADLB"]], "PARAMCD"),
-          "ALT"
-        ),
-        aval_var = teal.transform::choices_selected(
-          teal.transform::variable_choices(data[["ADLB"]], c("AVAL", "CHG")),
-          "AVAL"
-        ),
-        avalu_var = teal.transform::choices_selected(
-          teal.transform::variable_choices(data[["ADLB"]], c("AVALU")),
-          "AVALU",
-          fixed = TRUE
-        ),
-        id_var = teal.transform::choices_selected(
-          teal.transform::variable_choices(data[["ADLB"]], c("USUBJID")),
-          "USUBJID",
-          fixed = TRUE
-        ),
-        visit_var = teal.transform::choices_selected(
-          teal.transform::variable_choices(data[["ADLB"]], c("AVISIT", "ATOXGR")),
-          "AVISIT"
-        ),
-        baseline_var = teal.transform::choices_selected(
-          teal.transform::variable_choices(data[["ADLB"]], c("BASE")),
-          "BASE",
-          fixed = TRUE
-        ),
+        paramcd_var = teal.picks::variables("PARAMCD", fixed = TRUE),
+        paramcd_value = paramcd_value,
+        aval_var = teal.picks::variables(c("AVAL", "CHG"), multiple = FALSE),
+        avalu_var = teal.picks::variables("AVALU", fixed = TRUE),
+        id_var = teal.picks::variables("USUBJID", fixed = TRUE),
+        visit_var = teal.picks::variables(c("AVISIT", "ATOXGR"), multiple = FALSE),
+        baseline_var = teal.picks::variables("BASE", fixed = TRUE),
         add_baseline_hline = FALSE,
         separate_by_obs = FALSE,
         suppress_legend = FALSE,
@@ -67,13 +60,13 @@ app_driver_tm_g_ipp <- function() {
 testthat::test_that("e2e - tm_g_ipp: Module initializes in teal without errors and produces plot output.", {
   skip_if_too_deep(5)
   app_driver <- app_driver_tm_g_ipp()
+  withr::defer(app_driver$stop())
   app_driver$expect_no_shiny_error()
   app_driver$expect_no_validation_error()
   testthat::expect_match(
     app_driver$get_active_module_plot_output("myplot"),
     "data:image/png;base64,"
   )
-  app_driver$stop()
 })
 
 testthat::test_that(
@@ -83,59 +76,68 @@ testthat::test_that(
   {
     skip_if_too_deep(5)
     app_driver <- app_driver_tm_g_ipp()
+    withr::defer(app_driver$stop())
+
     testthat::expect_equal(
       app_driver$get_text("a.nav-link.active"),
       "Individual Patient Plot"
     )
+
+    exported_values <- app_driver$get_values()$export
+    names(exported_values) <- gsub(
+      sprintf("%s-", app_driver$namespaces()$module(NULL)), "", names(exported_values),
+      fixed = TRUE
+    )
+
     testthat::expect_equal(
-      app_driver$get_active_module_input("arm_var-dataset_ADSL_singleextract-select"),
+      exported_values[["arm_var-picks_resolved"]]$variables$selected,
       "ARMCD"
     )
     testthat::expect_equal(
-      app_driver$get_active_module_input("arm_var-dataset_ADSL_singleextract-filter1-vals"),
+      exported_values[["arm_var-picks_resolved"]]$values$selected,
       "ARM A"
     )
     testthat::expect_equal(
-      app_driver$get_active_module_input("paramcd-dataset_ADLB_singleextract-filter1-col"),
+      exported_values[["paramcd-picks_resolved"]]$variables$selected,
       "PARAMCD"
     )
     testthat::expect_equal(
-      app_driver$get_active_module_input("paramcd-dataset_ADLB_singleextract-filter1-vals"),
+      exported_values[["paramcd-picks_resolved"]]$values$selected,
       "ALT"
     )
     testthat::expect_equal(
-      app_driver$get_active_module_input("visit_var-dataset_ADLB_singleextract-select"),
+      exported_values[["visit_var-picks_resolved"]]$variables$selected,
       "AVISIT"
     )
     testthat::expect_equal(
-      app_driver$get_active_module_input("aval_var-dataset_ADLB_singleextract-select"),
+      exported_values[["aval_var-picks_resolved"]]$variables$selected,
       "AVAL"
     )
     testthat::expect_equal(
-      app_driver$get_active_module_input("id_var-dataset_ADLB_singleextract-select"),
+      exported_values[["id_var-picks_resolved"]]$variables$selected,
       "USUBJID"
     )
     testthat::expect_equal(
-      app_driver$get_active_module_input("avalu_var-dataset_ADLB_singleextract-select"),
+      exported_values[["avalu_var-picks_resolved"]]$variables$selected,
       "AVALU"
     )
     testthat::expect_equal(
-      app_driver$get_active_module_input("baseline_var-dataset_ADLB_singleextract-select"),
+      exported_values[["baseline_var-picks_resolved"]]$variables$selected,
       "BASE"
     )
     testthat::expect_false(app_driver$get_active_module_input("add_baseline_hline"))
     testthat::expect_false(app_driver$get_active_module_input("separate_by_obs"))
     testthat::expect_false(app_driver$get_active_module_input("suppress_legend"))
     testthat::expect_true(app_driver$get_active_module_input("add_avalu"))
-    app_driver$stop()
   }
 )
 
 testthat::test_that("e2e - tm_g_ipp: Selecting arm_var changes plot and doesn't throw validation errors.", {
   skip_if_too_deep(5)
   app_driver <- app_driver_tm_g_ipp()
+  withr::defer(app_driver$stop())
   plot_before <- app_driver$get_active_module_plot_output("myplot")
-  app_driver$set_active_module_input("arm_var-dataset_ADSL_singleextract-filter1-vals", "ARM B")
+  set_teal_picks_slot(app_driver, "arm_var", "values", "ARM B")
   testthat::expect_false(
     identical(
       plot_before,
@@ -143,29 +145,28 @@ testthat::test_that("e2e - tm_g_ipp: Selecting arm_var changes plot and doesn't 
     )
   )
   app_driver$expect_no_validation_error()
-  app_driver$stop()
 })
 
 testthat::test_that("e2e - tm_g_ipp: Deselecting arm_var column throws validation error.", {
   skip_if_too_deep(5)
   app_driver <- app_driver_tm_g_ipp()
-  app_driver$set_active_module_input("arm_var-dataset_ADSL_singleextract-filter1-vals", NULL)
+  withr::defer(app_driver$stop())
+  set_teal_picks_slot(app_driver, "arm_var", "values", character(0L))
   testthat::expect_identical(app_driver$get_active_module_plot_output("myplot"), character(0))
-  testthat::expect_identical(
-    app_driver$get_text(
-      app_driver$namespaces(TRUE)$module("arm_var-dataset_ADSL_singleextract-filter1-vals_input > div > span")
-    ),
-    "Please select Arm filter."
-  )
   app_driver$expect_validation_error()
-  app_driver$stop()
+  testthat::expect_match(
+    app_driver$get_text(app_driver$namespaces(TRUE)$module("myplot-plot_main")),
+    "Arm variable is empty.",
+    fixed = TRUE
+  )
 })
 
 testthat::test_that("e2e - tm_g_ipp: Selecting paramcd changes plot and doesn't throw validation errors.", {
   skip_if_too_deep(5)
   app_driver <- app_driver_tm_g_ipp()
+  withr::defer(app_driver$stop())
   plot_before <- app_driver$get_active_module_plot_output("myplot")
-  app_driver$set_active_module_input("paramcd-dataset_ADLB_singleextract-filter1-vals", "CRP")
+  set_teal_picks_slot(app_driver, "paramcd", "values", "CRP")
   testthat::expect_false(
     identical(
       plot_before,
@@ -173,29 +174,28 @@ testthat::test_that("e2e - tm_g_ipp: Selecting paramcd changes plot and doesn't 
     )
   )
   app_driver$expect_no_validation_error()
-  app_driver$stop()
 })
 
 testthat::test_that("e2e - tm_g_ipp: Deselecting paramcd throws validation error.", {
   skip_if_too_deep(5)
   app_driver <- app_driver_tm_g_ipp()
-  app_driver$set_active_module_input("paramcd-dataset_ADLB_singleextract-filter1-vals", NULL)
+  withr::defer(app_driver$stop())
+  set_teal_picks_slot(app_driver, "paramcd", "values", character(0L))
   testthat::expect_identical(app_driver$get_active_module_plot_output("myplot"), character(0))
-  testthat::expect_identical(
-    app_driver$get_text(
-      app_driver$namespaces(TRUE)$module("paramcd-dataset_ADLB_singleextract-filter1-vals_input > div > span")
-    ),
-    "Please select Parameter filter."
-  )
   app_driver$expect_validation_error()
-  app_driver$stop()
+  testthat::expect_match(
+    app_driver$get_text(app_driver$namespaces(TRUE)$module("myplot-plot_main")),
+    "`Select Parameter` field is empty",
+    fixed = TRUE
+  )
 })
 
 testthat::test_that("e2e - tm_g_ipp: Selecting visit_var changes plot and doesn't throw validation errors.", {
   skip_if_too_deep(5)
   app_driver <- app_driver_tm_g_ipp()
+  withr::defer(app_driver$stop())
   plot_before <- app_driver$get_active_module_plot_output("myplot")
-  app_driver$set_active_module_input("visit_var-dataset_ADLB_singleextract-select", "ATOXGR")
+  set_teal_picks_slot(app_driver, "visit_var", "variables", "ATOXGR")
   testthat::expect_false(
     identical(
       plot_before,
@@ -203,29 +203,28 @@ testthat::test_that("e2e - tm_g_ipp: Selecting visit_var changes plot and doesn'
     )
   )
   app_driver$expect_no_validation_error()
-  app_driver$stop()
 })
 
 testthat::test_that("e2e - tm_g_ipp: Deselecting visit_var throws validation error.", {
   skip_if_too_deep(5)
   app_driver <- app_driver_tm_g_ipp()
-  app_driver$set_active_module_input("visit_var-dataset_ADLB_singleextract-select", NULL)
+  withr::defer(app_driver$stop())
+  set_teal_picks_slot(app_driver, "visit_var", "variables", character(0L))
   testthat::expect_identical(app_driver$get_active_module_plot_output("myplot"), character(0))
-  testthat::expect_identical(
-    app_driver$get_text(
-      app_driver$namespaces(TRUE)$module("visit_var-dataset_ADLB_singleextract-select_input > div > span")
-    ),
-    "A Timepoint Variable must be selected"
-  )
   app_driver$expect_validation_error()
-  app_driver$stop()
+  testthat::expect_match(
+    app_driver$get_text(app_driver$namespaces(TRUE)$module("myplot-plot_main")),
+    "A Timepoint Variable must be selected",
+    fixed = TRUE
+  )
 })
 
 testthat::test_that("e2e - tm_g_ipp: Selecting aval_var changes plot and doesn't throw validation errors.", {
   skip_if_too_deep(5)
   app_driver <- app_driver_tm_g_ipp()
+  withr::defer(app_driver$stop())
   plot_before <- app_driver$get_active_module_plot_output("myplot")
-  app_driver$set_active_module_input("aval_var-dataset_ADLB_singleextract-select", "CHG")
+  set_teal_picks_slot(app_driver, "aval_var", "variables", "CHG")
   testthat::expect_false(
     identical(
       plot_before,
@@ -233,27 +232,26 @@ testthat::test_that("e2e - tm_g_ipp: Selecting aval_var changes plot and doesn't
     )
   )
   app_driver$expect_no_validation_error()
-  app_driver$stop()
 })
 
 testthat::test_that("e2e - tm_g_ipp: Deselecting aval_var throws validation error.", {
   skip_if_too_deep(5)
   app_driver <- app_driver_tm_g_ipp()
-  app_driver$set_active_module_input("aval_var-dataset_ADLB_singleextract-select", NULL)
+  withr::defer(app_driver$stop())
+  set_teal_picks_slot(app_driver, "aval_var", "variables", character(0L))
   testthat::expect_identical(app_driver$get_active_module_plot_output("myplot"), character(0))
-  testthat::expect_identical(
-    app_driver$get_text(
-      app_driver$namespaces(TRUE)$module("aval_var-dataset_ADLB_singleextract-select_input > div > span")
-    ),
-    "A Parameter values over Time must be selected"
-  )
   app_driver$expect_validation_error()
-  app_driver$stop()
+  testthat::expect_match(
+    app_driver$get_text(app_driver$namespaces(TRUE)$module("myplot-plot_main")),
+    "A Parameter values over Time must be selected",
+    fixed = TRUE
+  )
 })
 
 testthat::test_that("e2e - tm_g_ipp: Changing add_baseline_hline changes plot and doesn't throw validation errors.", {
   skip_if_too_deep(5)
   app_driver <- app_driver_tm_g_ipp()
+  withr::defer(app_driver$stop())
   plot_before <- app_driver$get_active_module_plot_output("myplot")
   app_driver$set_active_module_input("add_baseline_hline", TRUE)
   testthat::expect_false(
@@ -263,11 +261,12 @@ testthat::test_that("e2e - tm_g_ipp: Changing add_baseline_hline changes plot an
     )
   )
   app_driver$expect_no_validation_error()
-  app_driver$stop()
 })
+
 testthat::test_that("e2e - tm_g_ipp: Changing separate_by_obs changes plot and doesn't throw validation errors.", {
   skip_if_too_deep(5)
   app_driver <- app_driver_tm_g_ipp()
+  withr::defer(app_driver$stop())
   plot_before <- app_driver$get_active_module_plot_output("myplot")
   app_driver$set_active_module_input("separate_by_obs", TRUE)
   testthat::expect_false(
@@ -277,12 +276,12 @@ testthat::test_that("e2e - tm_g_ipp: Changing separate_by_obs changes plot and d
     )
   )
   app_driver$expect_no_validation_error()
-  app_driver$stop()
 })
 
 testthat::test_that("e2e - tm_g_ipp: Changing suppress_legend changes plot and doesn't throw validation errors.", {
   skip_if_too_deep(5)
   app_driver <- app_driver_tm_g_ipp()
+  withr::defer(app_driver$stop())
   plot_before <- app_driver$get_active_module_plot_output("myplot")
   app_driver$set_active_module_input("suppress_legend", TRUE)
   testthat::expect_false(
@@ -292,12 +291,12 @@ testthat::test_that("e2e - tm_g_ipp: Changing suppress_legend changes plot and d
     )
   )
   app_driver$expect_no_validation_error()
-  app_driver$stop()
 })
 
 testthat::test_that("e2e - tm_g_ipp: Changing add_avalu changes plot and doesn't throw validation errors.", {
   skip_if_too_deep(5)
   app_driver <- app_driver_tm_g_ipp()
+  withr::defer(app_driver$stop())
   plot_before <- app_driver$get_active_module_plot_output("myplot")
   app_driver$set_active_module_input("add_avalu", FALSE)
   testthat::expect_false(
@@ -307,5 +306,4 @@ testthat::test_that("e2e - tm_g_ipp: Changing add_avalu changes plot and doesn't
     )
   )
   app_driver$expect_no_validation_error()
-  app_driver$stop()
 })
