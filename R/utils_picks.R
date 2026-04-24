0549da4856989090ec33d7c95255724d38eb1e77
André Veríssimo <211358+averissimo@users.noreply.github.com>
2026-04-24 12:54:24 +0100

    pr: feedback from @m7pr

--- R/utils_picks.R
+++ R/utils_picks.R
@@ -42,7 +42,7 @@ migrate_choices_selected_to_variables <- function(x, # nolint: object_length_lin
   } else {
     if (!is.null(multiple) && !identical(attr(x, "multiple", exact = TRUE), multiple)) {
       stop(
-        sprintf("Multiple variables are not allowed for %s.", arg_name),
+        sprintf("`multiple` metadata does not match the requirement for %s.", arg_name),
         sprintf(" Please set multiple = %s in the picks object.", multiple)
       )
     }
@@ -119,7 +119,7 @@ migrate_value_choices_to_picks <- function(x, # nolint: object_length_linter.
   if (inherits(x, "picks")) {
     if (!is.null(multiple) && !identical(attr(x$values, "multiple", exact = TRUE), multiple)) {
       stop(
-        sprintf("Multiple variables are not allowed for %s.", arg_name),
+        sprintf("`multiple` metadata does not match the requirement for %s.", arg_name),
         sprintf(" Please set multiple = %s in the picks object.", multiple)
       )
     }
