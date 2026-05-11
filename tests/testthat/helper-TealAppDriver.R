02cda8df9eb646cb569f8ab3e80d6a7f53cb4111
Marcin <133694481+m7pr@users.noreply.github.com>
2026-05-11 15:05:46 +0200

    Update helper-TealAppDriver.R
    Signed-off-by: Marcin <133694481+m7pr@users.noreply.github.com>

--- tests/testthat/helper-TealAppDriver.R
+++ tests/testthat/helper-TealAppDriver.R
@@ -172,7 +172,7 @@ init_teal_app_driver <- function(...) {
 # `updateSelectInput`). Uses one [`AppDriver$wait_for_js()`] instead of polling `wait_for_idle()`.
 wait_until_nonempty_active_module_input <- function(app_driver, input_id) { # nolint: object_length_linter.
   checkmate::assert_string(input_id)
-  full_id <- app_driver$namespaces(TRUE)$module(input_id)
+  full_id <- app_driver$namespaces(FALSE)$module(input_id)
   id_lit <- .teal_picks_js_id_literal(full_id)
   app_driver$wait_for_js(sprintf(
     paste0(
