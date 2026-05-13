fdd23b6ab6b6d0b1930d720434802ee6792a44cc
m7pr <marcin.kosinski.mk1@roche.com>
2026-05-13 11:02:24 +0200

    fix tests

--- tests/testthat/test-shinytest2-tm_g_km.R
+++ tests/testthat/test-shinytest2-tm_g_km.R
@@ -51,7 +51,11 @@ app_driver_tm_g_km <- function() {
           selected = "SEX",
           multiple = TRUE
         ),
-        facet_var = NULL,
+        facet_var = teal.picks::variables(
+          choices = c("SEX", "BMRKR2"),
+          selected = NULL,
+          multiple = FALSE
+        ),
         time_unit_var = teal.picks::variables("VALUE_UNIT", fixed = TRUE),
         aval_var = teal.picks::variables("ANALYSIS_VAL", fixed = TRUE),
         cnsr_var = teal.picks::variables("CENSORING", fixed = TRUE),
